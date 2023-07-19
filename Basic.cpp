368af8bb0da826905bf7b7f57b2772629c3e43c3
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-07-19 23:08:06 +0545

    Basics of C++

---
+++ Basic.cpp
@@ -0,0 +1,26 @@
+#include<iostream> //Header file
+using namespace std;
+int main(){
+    cout << "Hello World!";
+    //Outputting a text
+    cout << 25;
+    // Outputting a Number
+
+    cout << "Hello This is first line" << endl;
+    // endl command adds a new line
+    cout << "This is the second line" << endl;
+
+    string name= "Subodh";
+    //creates a variable called name of type string and assigns value as "Subodh"
+    cout << name << endl; 
+    //Prints value as Subodh
+    name = "Subodh Ghimire";
+    // Now, the name holds the value "Subodh Ghimire";
+    cout << name << endl ;
+    // Prints the value as Subodh Ghimire
+
+    // string name= "Subodh Ghimire";
+    // cout << name;
+    // Another way of declaring and assigning value to a variable
+
+}
