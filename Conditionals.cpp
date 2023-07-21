0eb7cf4aedc4d2af3fcfe20f24b21522b72d8906
Subodh Ghimire <ghimiresubodh59@gmail.com>
2023-07-21 21:55:52 +0545

    Conditions

--- Conditionals.cpp
+++ Conditionals.cpp
@@ -24,6 +24,22 @@ int main(){
     else {
         cout << "x = " << 1; 
     }
+
+    //Switch Case
+    int choice = 8;
+    switch(choice) {
+        case 1:
+            cout << "Coffee";
+        break;
+        case 2:
+            cout << "Tea";
+        break;
+        case 3:
+            cout << "Water";
+        break;
+    default:
+        cout << "Invalid Choice";
+}
 }
 
 /*--->> Multiple Inputs */
@@ -51,4 +67,35 @@ int main(){
 //     }else{
 //         cout << "Not allowed";
 //     }
+// }
+
+/*--->> Switch Case */
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int choice;
+//     cin >> choice;
+//     /* Coffee Types:
+//     1. Espresso
+//     2. Americano
+//     3. Cappuccino
+//     4. Latte
+//     */
+//    switch(choice){
+//     case 1:
+//         cout << "Espresso";
+//         break;
+//     case 2:
+//         cout << "Americano";
+//         break;
+//     case 3:
+//         cout << "Cappuccino";
+//         break;
+//     case 4:
+//         cout << "Latte";
+//         break;
+//     default:
+//         cout << "Invalid Choice";
+//    }
 // }
