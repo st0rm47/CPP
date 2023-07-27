3d2128cfdca1dd7ff1a29c6286f75299246475ea
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-07-27 10:53:29 +0545

    lab2

--- lab2.cpp
+++ lab2.cpp
@@ -1,3 +1,40 @@
+// //Program to add 2 complex numbers
+
+// #include<iostream>
+// using namespace std;
+// class complex{  //class
+//     private:
+//         int real;
+//         int imaginary;
+//     public:
+//         void input(){
+//         cout << "Enter the real number";
+//         cin >> real;
+//         cout << "Enter the imaginary number";
+//         cin >> imaginary;
+//         }
+//     void sum(complex a, complex b);
+//     void showdata();
+// };
+// void complex :: sum(complex a, complex b){  //Function to add 2 complex numbers
+//     real=a.real+b.real;
+//     imaginary=a.imaginary + b.imaginary; 
+// }
+// void complex :: showdata(){     //Function to display the complex data
+//     cout << "The sum is " << real << " + " << imaginary << "i"<< endl;
+// }
+// int main(){
+//     complex c1,c2,c3;
+//     c1.input();     // function call
+//     c2.input();     // function call
+//     c3.sum(c1,c2);  // function call
+//     c3.showdata();  // function call
+//     return 0;
+// }
+
+
+
+
 //Program to add 2 complex numbers
 
 #include<iostream>
@@ -13,21 +50,28 @@ class complex{  //class
         cout << "Enter the imaginary number";
         cin >> imaginary;
         }
-    void sum(complex a, complex b);
+    complex sum(complex a, complex b);
     void showdata();
 };
-void complex :: sum(complex a, complex b){  //Function to add 2 complex numbers
-    real=a.real+b.real;
-    imaginary=a.imaginary + b.imaginary; 
+complex complex :: sum(complex a, complex b){  //Function to add 2 complex numbers
+    
+    complex d;      //local variable
+    d.real=a.real+b.real;
+    d.imaginary=a.imaginary + b.imaginary; 
+    return d;
 }
 void complex :: showdata(){     //Function to display the complex data
     cout << "The sum is " << real << " + " << imaginary << "i"<< endl;
 }
 int main(){
-    complex c1,c2,c3;
+    complex c1,c2,c3,c4;
     c1.input();     // function call
     c2.input();     // function call
-    c3.sum(c1,c2);  // function call
+    // c3.sum(c1,c2);  // function call
+
+    c3=c3.sum(c1,c2); //prints the value from function complex :: sum
     c3.showdata();  // function call
+
+    
     return 0;
 }
