05690b6b957ec4de162f02a3c10a1055b5a33641
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-07-27 07:57:57 +0545

    Functions

--- functions.cpp
+++ functions.cpp
@@ -33,39 +33,39 @@ using namespace std;
 
 
 /*--->>ChatBot using Function*/
-#include <iostream>
-using namespace std;
-void bot(int mode,string name) {
-    if (mode ==1){
-        cout << "Welcome, " << name << "!";
-    }
-    else if (mode == 2){
-        cout << "Goodbye, " << name << "!";
-    }
-    else
-        cout << "Try again";
-    }  
-int main() {
-    int mode;
-    cin >> mode;
-    string name;
-    cin >> name;
+// #include <iostream>
+// using namespace std;
+// void bot(int mode,string name) {
+//     if (mode ==1){
+//         cout << "Welcome, " << name << "!";
+//     }
+//     else if (mode == 2){
+//         cout << "Goodbye, " << name << "!";
+//     }
+//     else
+//         cout << "Try again";
+//     }  
+// int main() {
+//     int mode;
+//     cin >> mode;
+//     string name;
+//     cin >> name;
 
-    bot(mode, name);
-}
+//     bot(mode, name);
+// }
 
 
 /*--->>Days to Seconds*/
-#include <iostream>
-using namespace std;
-int toSeconds(int days){
-    int second= days*24*60*60;
-    return second;
-}
-int main() {
-    int days;
-    cin >> days;
+// #include <iostream>
+// using namespace std;
+// int toSeconds(int days){
+//     int second= days*24*60*60;
+//     return second;
+// }
+// int main() {
+//     int days;
+//     cin >> days;
 
-    int seconds = toSeconds(days);
-    cout << seconds;
-}
+//     int seconds = toSeconds(days);
+//     cout << seconds;
+// }
