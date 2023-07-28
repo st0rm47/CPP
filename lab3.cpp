150988694ff8e441a41ea3e7f7068a5df822da16
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-07-28 11:25:55 +0545

    lab3

---
+++ lab3.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class two; //Second class declaration
+class one{
+    //Data is private ---> Data hiding
+    int x,y;    //Data members
+    public:
+        void getdata(){ //member function
+           
+           cout << "Enter values of x and y for class one";
+            cin >> x >> y;
+        }
+        friend int display(one a,one );  //Friend function declaration with keyword friend and argument as object
+
+};      //Data and function in same place ---> Data Encapsulation
+
+class two{
+    int x,y;    //Data members
+    public:
+        void getdata(){ //member function
+           
+           cout << "Enter values of x and y for class two";
+            cin >> x >> y;
+        }
+        friend int display(one a,one);  //Friend function declaration with keyword friend and argument as object
+
+};
+int display(one a,one b){         //non-member function
+    return a.x+b.x;
+}
+int main(){
+one a;      //object
+two b;
+a.getdata();
+b.getdata();
+cout << display(a,b);
+}
