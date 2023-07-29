e113b9ebfcae20158211ad0cca1febd95cabbe49
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-07-29 21:27:17 +0545

    Class

--- Classes.cpp
+++ Classes.cpp
@@ -7,11 +7,17 @@ Objects are created using classes.*/
     using namespace std;
     class myname{           // Declaring a class named myname using keyword: class
         public:             // Access Specifier
+
+        myname(){           //Constructor 
+            cout << 45 << endl;
+        }
         void name(){        // Member function inside a class
             cout << "Subodh";
         }
     };
     int main(){
         myname a;          // Object to access the data inside a class
+
+        //When object is made constructor are executed automatically
         a.name();          // object with dot(.) should be used to access inside a class
     }
