af4544c13b24ab50995b7401350a7d7f8ab33d42
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-07-30 23:45:28 +0545

    Classes

--- Classes.cpp
+++ Classes.cpp
@@ -18,7 +18,7 @@ Objects are created using classes.*/
     int main(){
         myname a;          // Object to access the data inside a class
 
-        //When object is made constructor are executed automatically even without calling
+        //When object is made constructor are executed automatically
         a.name();          // object with dot(.) should be used to access inside a class
     }
 
