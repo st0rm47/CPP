551b4a98cb8248531e20443614b857e5666a4720
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-07-31 20:57:58 +0545

    Function

--- Function_Part2.cpp
+++ Function_Part2.cpp
@@ -1,7 +1,26 @@
-//Pass By value
+// //Pass By value
+// #include<iostream>
+// using namespace std;
+// void swap(int a,int b){
+//     int temp;
+//     temp=a;
+//     a=b;
+//     b=temp;
+//     }
+// int main(){
+//     int x=5,y=3;
+//     swap(x,y);
+//     cout << "After Function Call" << endl;
+//     cout << "x = " << x << endl;
+//     cout << "y = " << y;
+
+//     /*Values of x and y were passed but not variables x and y*/
+// }
+
+//Pass By reference
 #include<iostream>
 using namespace std;
-void swap(int a,int b){
+void swap(int &a,int &b){
     int temp;
     temp=a;
     a=b;
@@ -13,4 +32,7 @@ int main(){
     cout << "After Function Call" << endl;
     cout << "x = " << x << endl;
     cout << "y = " << y;
+
+    /*Variables x and y were passed but not values.
+    When function is called, it points to the memory location of x and y*/
 }
