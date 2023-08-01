6181e491a0bbea0b679114cc645f3aa4d0cc0794
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-01 10:34:46 +0545

    Lab4

---
+++ lab4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class newone{
+    int x,y;
+        friend class newstone;
+};
+class newstone{
+    public:
+    void getdata(newone a){
+        cout << "Enter values of x and y: ";
+        cin >> a.x >> a.y;
+        cout << a.x+a.y;
+    
+    }
+};
+int main(){
+    newone a;
+    newstone m;
+    m.getdata(a);
+}
