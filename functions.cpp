3f707c32fdf641ead10258cc8237f23d4820c911
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-01 21:25:57 +0545

    Functions

--- functions.cpp
+++ functions.cpp
@@ -33,39 +33,99 @@ using namespace std;
 
 
 /*--->>ChatBot using Function*/
-// #include <iostream>
-// using namespace std;
-// void bot(int mode,string name) {
-//     if (mode ==1){
-//         cout << "Welcome, " << name << "!";
-//     }
-//     else if (mode == 2){
-//         cout << "Goodbye, " << name << "!";
-//     }
-//     else
-//         cout << "Try again";
-//     }  
-// int main() {
-//     int mode;
-//     cin >> mode;
-//     string name;
-//     cin >> name;
+#include <iostream>
+using namespace std;
+void bot(int mode,string name) {
+    if (mode ==1){
+        cout << "Welcome, " << name << "!";
+    }
+    else if (mode == 2){
+        cout << "Goodbye, " << name << "!";
+    }
+    else
+        cout << "Try again";
+    }  
+int main() {
+    int mode;
+    cin >> mode;
+    string name;
+    cin >> name;
 
-//     bot(mode, name);
-// }
+    bot(mode, name);
+}
 
 
 /*--->>Days to Seconds*/
-// #include <iostream>
-// using namespace std;
-// int toSeconds(int days){
-//     int second= days*24*60*60;
-//     return second;
-// }
-// int main() {
-//     int days;
-//     cin >> days;
+#include <iostream>
+using namespace std;
+int toSeconds(int days){
+    int second= days*24*60*60;
+    return second;
+}
+int main() {
+    int days;
+    cin >> days;
 
-//     int seconds = toSeconds(days);
-//     cout << seconds;
-// }
+    int seconds = toSeconds(days);
+    cout << seconds;
+}
+
+
+// //Pass By value
+#include<iostream>
+using namespace std;
+void swap(int a,int b){
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    }
+int main(){
+    int x=5,y=3;
+    swap(x,y);
+    cout << "After Function Call" << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y;
+
+    /*Values of x and y were passed but not variables x and y*/
+}
+
+//Pass By reference
+#include<iostream>
+using namespace std;
+void swap(int &a,int &b){
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    }
+int main(){
+    int x=5,y=3;
+    swap(x,y);
+    cout << "After Function Call" << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y;
+
+    /*Variables x and y were passed but not values.
+    When function is called, it points to the memory location of x and y*/
+}
+
+//Pass By Pointers
+#include<iostream>
+using namespace std;
+void swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+    }
+int main(){
+    int x=5,y=3;
+    swap(&x,&y);
+    cout << "After Function Call" << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y;
+
+    /*Address of variables x and y were passed but not values.
+    When function is called, it points to the memory location of x and y*/
+}
