5de2f1fdfd589d09da070c72ceb15bb0f091184a
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-03 10:50:52 +0545

    Constructor

--- Constructor.cpp
+++ Constructor.cpp
@@ -1,68 +1,72 @@
-// //--->>Constructor
-//     /*Special member function
-//      Initializes object 
-//     Name same as class name */
+//--->>Constructor
+    /*Special member function
+     Initializes object 
+    Name same as class name */
 
-// //--->> Default Constructor
-// #include<iostream>
-// using namespace std;
-// class naya {        //class
-//     int a,b;
-//     public:
-//         naya(){     //constructor
-//             cout << "Enter values of a and b: ";
-//             cin >> a >> b;
-//             cout << a << endl << b;
-//         }
+//--->> Default Constructor
+//1.
+#include<iostream>
+using namespace std;
+class naya {        //class
+    int a,b;
+    public:
+        naya(){     //constructor
+            cout << "Enter values of a and b: ";
+            cin >> a >> b;
+            cout << a << endl << b;
+        }
+};
+int main(){
+    naya n;         //object created
+}
 
-// };
-// int main(){
-//     naya n;         //object created
-// }
-
-// #include<iostream>
-// using namespace std;
-// class test{
-//     int x,y;
-//     public:
-//         test(){     //constructor
+//2.
+#include<iostream>
+using namespace std;
+class test{
+    int x,y;
+    public:
+        test(){     //constructor
         
-//             x=5;
-//             y=10;
-//         }
-//         void show(){
-//             cout << x << endl << y;
+            x=5;
+            y=10;
+        }
+        void show(){
+            cout << x << endl << y;
 
-//         }
-// };
-// int main(){
-//     test t;
-//     t.show();
-// }
+        }
+};
+int main(){
+    test t;
+    t.show();
+}
 
-// //--->> Parametrized Constructor
-// #include <iostream>
-// #include <string>
-// using namespace std;
-// class Painting {        //class
-//     private:
-//         string name;
-//     public:
-//         Painting(string name){      //constructor
+
+
+//--->> Parametrized Constructor
+//1.
+#include <iostream>
+#include <string>
+using namespace std;
+class Painting {        //class
+    private:
+        string name;
+    public:
+        Painting(string name){      //constructor
         
-//             cout << name;
-//         }
-// };
-// int main() {
-//     string name;
-//     cin >> name;
+            cout << name;
+        }
+};
+int main() {
+    string name;
+    cin >> name;
 
-//     Painting painting(name);        //used constructor to pass the value
+    Painting painting(name);        //used constructor to pass the value
 
-//     return 0;
-// }
+    return 0;
+}
 
-
+//2.
 #include<iostream>
 using namespace std;
 class naya {        //class
@@ -73,7 +77,6 @@ class naya {        //class
             y=b;
             cout << x << "\t" << y << endl;
         }
-
 };
 int main(){
     naya n(3,4);         //object created
