dd8fb04fc68312c334d627197eb4a032bfe89f7b
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-03 10:45:49 +0545

    Constructor

--- Constructor.cpp
+++ Constructor.cpp
@@ -1,27 +1,23 @@
-//--->>Constructor
-    /*Special member function
-     Initializes object 
-    Name same as class name */
+// //--->>Constructor
+//     /*Special member function
+//      Initializes object 
+//     Name same as class name */
 
-// #include <iostream>
-// #include <string>
+// //--->> Default Constructor
+// #include<iostream>
 // using namespace std;
-// class Painting {        //class
-//     private:
-//         string name;
+// class naya {        //class
+//     int a,b;
 //     public:
-//         Painting(string name){      //constructor
-        
-//             cout << name;
+//         naya(){     //constructor
+//             cout << "Enter values of a and b: ";
+//             cin >> a >> b;
+//             cout << a << endl << b;
 //         }
+
 // };
-// int main() {
-//     string name;
-//     cin >> name;
-
-//     Painting painting(name);        //used constructor to pass the value
-
-//     return 0;
+// int main(){
+//     naya n;         //object created
 // }
 
 // #include<iostream>
@@ -44,20 +40,43 @@
 //     t.show();
 // }
 
+// //--->> Parametrized Constructor
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// class Painting {        //class
+//     private:
+//         string name;
+//     public:
+//         Painting(string name){      //constructor
+        
+//             cout << name;
+//         }
+// };
+// int main() {
+//     string name;
+//     cin >> name;
+
+//     Painting painting(name);        //used constructor to pass the value
+
+//     return 0;
+// }
 
 
 #include<iostream>
 using namespace std;
 class naya {        //class
-    int a,b;
+    int x,y;
     public:
-        naya(){     //constructor
-            cout << "Enter values of a and b: ";
-            cin >> a >> b;
-            cout << a << endl << b;
+        naya(int a,int b){     //constructor with arguments
+            x=a;
+            y=b;
+            cout << x << "\t" << y << endl;
         }
 
 };
 int main(){
-    naya n;         //object created
+    naya n(3,4);         //object created
+    naya n2(4,5);
+    return 0;
 }
