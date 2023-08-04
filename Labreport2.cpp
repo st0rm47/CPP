376213c820bfb80286da38c2fdd295b6d6a2fb25
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-04 07:57:27 +0545

    Lab report 2

--- Labreport2.cpp
+++ Labreport2.cpp
@@ -1,76 +1,108 @@
-//Using friend function
-
-#include<iostream>
-using namespace std;
-class friendfun
-{
-    int x,y;
-    public:
-    void getdata()
-    {
-        cout<<"Enter x and y: ";
-        cin>>x>>y;
-    }
-    friend friendfun show (friendfun a);
-};
-friendfun show(friendfun a)
-{
-    cout<<a.x<<endl<<a.y;
-    return a;
-}
-
-int main (){
-    int c;
-    friendfun a;
-    a.getdata();
-    show(a);
-    return 0;
-}
+// //WAP to show the concept of friend
+// #include<iostream>
+// using namespace std;
+// class friendfun{
+//     int a,b;
+//     public:
+//     void getdata()
+//     {
+//         cout <<"Enter values of a and b: ";
+//         cin >> a >> b;
+//     }
+//     friend friendfun show (friendfun f);
+// };
+// friendfun show(friendfun f){
+//     cout << f.a << endl << f.b;
+//     return f;
+// }
+// int main (){
+//     int c;
+//     friendfun f;
+//     f.getdata();
+//     show(f);
+//     return 0;
+// }
 
 
 
-//WAP to show the friend function acting as bridge between two classes.
-#include<iostream>
-using namespace std;
+// //WAP to show the friend function acting as bridge between two classes.
+// // #include<iostream>
+// // using namespace std;
 
-class two; // pre-declaration
+// // class two; // pre-declaration
 
-class one {
-    int x;  
+// // class one {
+// //     int x;  
 
-public:
-    void access(){
-        cout<<"enter the value of x: ";
-        cin>>x;
-    }
-    friend void bridge(one, two);
-}; //end of class one
+// // public:
+// //     void access(){
+// //         cout<<"enter the value of x: ";
+// //         cin>>x;
+// //     }
+// //     friend void bridge(one, two);
+// // }; //end of class one
 
-class two {
-    int y;
+// // class two {
+// //     int y;
 
-public:
-    void access() {
-        cout<<endl<<"Enter the value of y: ";
-        cin>>y;
-    }
-    friend void bridge(one,two);
-};//end of class two
+// // public:
+// //     void access() {
+// //         cout<<endl<<"Enter the value of y: ";
+// //         cin>>y;
+// //     }
+// //     friend void bridge(one,two);
+// // };//end of class two
 
-void bridge(one objA, two objB) {
-    int z;
-    z= objA.x+objB.y;
-    cout<<endl<<"Sum is: "<<z;
-}
+// // void bridge(one objA, two objB) {
+// //     int z;
+// //     z= objA.x+objB.y;
+// //     cout<<endl<<"Sum is: "<<z;
+// // }
 
 
-int main() {
-   one obj1;
-   two obj2;
-   obj1.access();
-   obj2.access();
-   bridge(obj1,obj2);
-}
+// // int main() {
+// //    one obj1;
+// //    two obj2;
+// //    obj1.access();
+// //    obj2.access();
+// //    bridge(obj1,obj2);
+// // }
+
+
+// #include<iostream>
+// using namespace std;
+// class two;
+// class one{
+//     int x,y;    
+//     public:
+//         void getdata(){           
+//            cout << "Enter values of x and y for class one : ";
+//             cin >> x >> y;
+//         }
+//         friend int display(one a,two b );  
+// };
+// class two{
+//     int x,y;
+//     public:
+//         void getdata(){            
+//            cout << "Enter values of x and y for class two :";
+//             cin >> x >> y;
+//         }
+//         friend int display(one a,two b); 
+// };
+// int display(one a,two b){     
+//     return a.x+b.x;
+//     return a.y+b.y;;
+// }
+// int main(){
+// one a;     
+// two b;
+// a.getdata();
+// b.getdata();
+// cout << display(a,b) << endl;
+// cout << display(a,b);
+// return 0;
+// }
 
 
 
@@ -78,7 +110,7 @@ int main() {
 #include <iostream>
 using namespace std;
 
-class one; // pre-declaration
+class newone; // pre-declaration
 
 class MyClass {
     int x;
