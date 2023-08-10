85a8713a86bfb1682a80db9aa22cdbd69772d3ff
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-10 10:24:55 +0545

    Lab 5

--- lab5.cpp
+++ lab5.cpp
@@ -68,43 +68,62 @@
 #include<iostream>
 using namespace std;
 
-class rectangle{
-    private:
-        int length;
-        int breadth;
-    public:
-        rectangle(){
-            length=0;
-            breadth=0;
-        }
-        rectangle(int x,int y){
-            length=x;
-            breadth=y;
-        }
-        rectangle(int z){
-            length=breadth=z;
-        }
-        int area(){
-            return length*breadth;
-        }
+class rectangle {
+private:
+    int length;
+    int breadth;
+
+public:
+    // Default constructor
+    rectangle() {
+        length = 0;
+        breadth = 0;
+    }
+
+    // Parameterized constructor for rectangle
+    rectangle(int x, int y) {
+        length = x;
+        breadth = y;
+    }
+
+    // Parameterized constructor for square
+    rectangle(int z) {
+        length = breadth = z;
+    }
+
+    // Method to calculate the area of the rectangle
+    int area() {
+        return length * breadth;
+    }
 };
-int main(){
-    int x,y;
-    cout << "Enter length and breadth";
+
+int main() {
+    int x, y;
+    
+    // Input length and breadth for the rectangle
+    cout << "Enter length and breadth: ";
     cin >> x >> y;
-    rectangle r1(x,y);
-    cout << "Area of Rectangle: " << r1.area() << endl;
+    
+    // Create a rectangle object using the parameterized constructor
+    rectangle r1(x, y);
+    
+    // Calculate and display the area of the rectangle
+    cout << "Area of rectangle r1: " << r1.area() << endl;
 
     int z;
-    cout << "Enter length: ";
+    
+    // Input side length for the square
+    cout << "Enter side length for square: ";
     cin >> z;
+    
+    // Create a square object using the parameterized constructor
     rectangle r2(z);
-
-    cout << "Area of Rectangle: " << r2.area() << endl;
+    
+    // Calculate and display the area of the square
+    cout << "Area of square r2: " << r2.area() << endl;
 
     return 0;
-
-
 }
+
 
 
