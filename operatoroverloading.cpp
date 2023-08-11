1af10b97c96a93d99c496a58fe9a7f2edb5d3374
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-11 11:00:52 +0545

    Operator Overloading

--- operatoroverloading.cpp
+++ operatoroverloading.cpp
@@ -45,21 +45,25 @@ class test{
         }
         test operator ++(){     //Pre-increment operator
            ++count;
-           test a;
-           a.count=count;
-           return a;
+           test a;              //Object of class test
+           a.count=count;       // Object value is set to the new updated value
+           return a;            // return the value of object with new value
         }
         // void operator ++ (int){     //Post-increment operator
         //     count++;
         // }
 };
 int main(){
-    test t;             // Object of clss test
-    t.display();       // Display the value of t as 0
+    test t1,t2;             // Object of clss test
+    t1.display();       // Display the value of t1 as 0
 
-    ++t;                // Increment t using pre-increment operator
-    t.display();       // Display the value of t as 1
+    ++t1;                // Increment t using pre-increment operator
+    t1.display();       // Display the value of t1 as 1
 
     // t++;                // Increment t using post-increment operator
-    // t.display();       // Display the value of t as 2
+    // t.display();       // Display the value of t1 as 2
+
+    t2=++t1;               // Set the value of new object as same as 1st object
+    t1.display();       // Display the value of t1 as 2
+    t2.display();           // Display the value of t2 as 2
 }
