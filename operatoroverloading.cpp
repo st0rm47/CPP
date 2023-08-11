ec211aae4bead404f215115b6225341dd045e3dc
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-11 10:55:43 +0545

    Operator Overloading

--- operatoroverloading.cpp
+++ operatoroverloading.cpp
@@ -1,3 +1,37 @@
+// /*--->> Operator Overloading */
+
+// /*1.Unary Operator Overloading without return type*/
+// #include<iostream>
+// using namespace std;
+// class test{
+//     int count;
+//     public:
+//         test(){
+//             count=0;
+//         }
+//         void display(){
+//             cout << count << endl;
+//         }
+//         void operator ++(){     //Pre-increment operator
+//            ++count;
+//         }
+//         void operator ++ (int){     //Post-increment operator
+//             count++;
+//         }
+// };
+// int main(){
+//     test t;             // Object of clss test
+//     t.display();       // Display the value of t as 0
+
+//     ++t;                // Increment t using pre-increment operator
+//     t.display();       // Display the value of t as 1
+
+//     t++;                // Increment t using post-increment operator
+//     t.display();       // Display the value of t as 2
+// }
+
+
+/*2.Unary Operator Overloading with return type*/
 #include<iostream>
 using namespace std;
 class test{
@@ -9,12 +43,15 @@ class test{
         void display(){
             cout << count << endl;
         }
-        void operator ++(){     //Pre-increment operator
+        test operator ++(){     //Pre-increment operator
            ++count;
+           test a;
+           a.count=count;
+           return a;
         }
-        void operator ++ (int){     //Post-increment operator
-            count++;
-        }
+        // void operator ++ (int){     //Post-increment operator
+        //     count++;
+        // }
 };
 int main(){
     test t;             // Object of clss test
@@ -23,6 +60,6 @@ int main(){
     ++t;                // Increment t using pre-increment operator
     t.display();       // Display the value of t as 1
 
-    t++;                // Increment t using post-increment operator
-    t.display();       // Display the value of t as 2
+    // t++;                // Increment t using post-increment operator
+    // t.display();       // Display the value of t as 2
 }
