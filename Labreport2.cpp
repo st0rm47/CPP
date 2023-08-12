6f49b4fd94d28ed8629c8c9659b7f7aa6d25b110
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-12 17:27:03 +0545

    Lab Report 2

--- Labreport2.cpp
+++ Labreport2.cpp
@@ -1,27 +1,27 @@
-// //WAP to show the concept of friend
-// #include<iostream>
-// using namespace std;
-// class friendfun{
-//     int a,b;
-//     public:
-//     void getdata()
-//     {
-//         cout <<"Enter values of a and b: ";
-//         cin >> a >> b;
-//     }
-//     friend friendfun show (friendfun f);
-// };
-// friendfun show(friendfun f){
-//     cout << f.a << endl << f.b;
-//     return f;
-// }
-// int main (){
-//     int c;
-//     friendfun f;
-//     f.getdata();
-//     show(f);
-//     return 0;
-// }
+//WAP to show the concept of friend
+#include<iostream>
+using namespace std;
+class friendfun{
+    int a,b;
+    public:
+    void getdata()
+    {
+        cout <<"Enter values of a and b: ";
+        cin >> a >> b;
+    }
+    friend friendfun show (friendfun f);
+};
+friendfun show(friendfun f){
+    cout << f.a << endl << f.b;
+    return f;
+}
+int main (){
+    int c;
+    friendfun f;
+    f.getdata();
+    show(f);
+    return 0;
+}
 
 
 
@@ -69,74 +69,93 @@
 // // }
 
 
-// #include<iostream>
-// using namespace std;
-// class two;
-// class one{
-//     int x,y;    
-//     public:
-//         void getdata(){           
-//            cout << "Enter values of x and y for class one : ";
-//             cin >> x >> y;
-//         }
-//         friend int display(one a,two b );  
-// };
-// class two{
-//     int x,y;
-//     public:
-//         void getdata(){            
-//            cout << "Enter values of x and y for class two :";
-//             cin >> x >> y;
-//         }
-//         friend int display(one a,two b); 
-// };
-// int display(one a,two b){     
-//     return a.x+b.x;
-//     return a.y+b.y;;
-// }
-// int main(){
-// one a;     
-// two b;
-// a.getdata();
-// b.getdata();
-// cout << display(a,b) << endl;
-// cout << display(a,b);
-// return 0;
-// }
+#include<iostream>
+using namespace std;
+class two;
+class one{
+    int x,y;    
+    public:
+        void getdata(){           
+           cout << "Enter values of x and y for class one : ";
+            cin >> x >> y;
+        }
+        friend int display(one a,two b );  
+};
+class two{
+    int x,y;
+    public:
+        void getdata(){            
+           cout << "Enter values of x and y for class two :";
+            cin >> x >> y;
+        }
+        friend int display(one a,two b); 
+};
+int display(one a,two b){     
+    return a.x+b.x;
+    return a.y+b.y;;
+}
+int main(){
+one a;     
+two b;
+a.getdata();
+b.getdata();
+cout << display(a,b) << endl;
+cout << display(a,b);
+return 0;
+}
 
 
 
 //WAP to show friend class.
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// class newone; // pre-declaration
+// class MyClass {
+//     int x;
+
+// public:
+//     void setX() {
+//         cout << "Enter the value of x: ";
+//         cin >> x;
+//     }
+
+//     friend class Display;
+// };
+
+// class Display {
+// public:
+//     void access(MyClass obj) {
+//         cout << "The value of x is: " << obj.x << endl;
+//     }
+// };
+
+// int main() {
+//     MyClass obj;
+//     obj.setX();
+
+//     Display friendObj;
+//     friendObj.access(obj);
+
+//     return 0;
+// }
+#include<iostream>
 using namespace std;
-
-class newone; // pre-declaration
-
-class MyClass {
-    int x;
-
-public:
-    void setX() {
-        cout << "Enter the value of x: ";
-        cin >> x;
-    }
-
-    friend class Display;
+class newone{
+    int x,y;
+        friend class newstone;
 };
-
-class Display {
-public:
-    void access(MyClass obj) {
-        cout << "The value of x is: " << obj.x << endl;
+class newstone{
+    public:
+    void getdata(newone a){
+        cout << "Enter values of x and y: ";
+        cin >> a.x >> a.y;
+        cout << a.x+a.y;
     }
 };
-
-int main() {
-    MyClass obj;
-    obj.setX();
-
-    Display friendObj;
-    friendObj.access(obj);
-
+int main(){
+    newone a;
+    newstone m;
+    m.getdata(a);
     return 0;
 }
