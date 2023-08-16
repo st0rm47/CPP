31fd68b73643ff6e73ef89e52842924794b91cc1
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-16 10:42:13 +0545

    Operator Overloading

--- Operatoroverloading_2.cpp
+++ Operatoroverloading_2.cpp
@@ -40,7 +40,10 @@ int main() {
 
     // Input real and imaginary parts for objects a and b
     a.getdata();
+    a.showdata();
+
     b.getdata();
+    b.showdata();
 
     // Use the overloaded + operator to add a and b, and store the result in c
     c = a + b;
