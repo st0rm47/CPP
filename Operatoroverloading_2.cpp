21654b50bf2578cc50508ad19cb7de16418c434c
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-17 21:55:17 +0545

    Operator Overloading

--- Operatoroverloading_2.cpp
+++ Operatoroverloading_2.cpp
@@ -37,3 +37,39 @@ int main() {
 
     c.showdata();
 }
+
+/* WAP to overload + operator to concatenate two strings.*/
+#include<iostream>
+using namespace std;
+class str{
+    private:
+        string name;
+    public:
+        str(){
+            name=" ";
+        }
+        str(string b){
+            name=b;
+        }
+        void getdata() {
+            cout << "Enter a string: ";
+            cin >> name;
+        }
+        void show(){
+            cout << name;
+        }
+        str operator +(str a){
+            str temp;
+            temp.name=name+a.name;
+            return temp;
+        }
+};
+int main(){
+    str a,b,c;
+    a.getdata();
+    b.getdata();
+    c=a+b;
+    c.show();
+}
+
+
