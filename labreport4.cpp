2604666bb95674f366249becdcb580a7980e67da
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-17 11:08:02 +0545

    Lab report 4

--- labreport4.cpp
+++ labreport4.cpp
@@ -30,35 +30,74 @@
 
 
 /*3. WAP to overload + operator to concatenate two strings.*/
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// class str{
+//     private:
+//         string name;
+//     public:
+//         str(){
+//             name=" ";
+//         }
+//         str(string b){
+//             name=b;
+//         }
+//         void getdata() {
+//             cout << "Enter a string: ";
+//             cin >> name;
+//         }
+//         void show(){
+//             cout << name;
+//         }
+//         str operator +(str a){
+//             str temp;
+//             temp.name=name+a.name;
+//             return temp;
+//         }
+// };
+// int main(){
+//     str a,b,c;
+//     a.getdata();
+//     b.getdata();
+//     c=a+b;
+//     c.show();
+// }
+
+
+/*4. WAP to two complex numbers using operator overload by friend function.*/
+#include <iostream>
 using namespace std;
-class str{
-    private:
-        string name;
-    public:
-        str(){
-            name=" ";
-        }
-        str(string b){
-            name=b;
-        }
-        void getdata() {
-            cout << "Enter a string: ";
-            cin >> name;
-        }
-        void show(){
-            cout << name;
-        }
-        str operator +(str a){
-            str temp;
-            temp.name=name+a.name;
-            return temp;
-        }
+class complex {
+private:
+    int real;
+    int imaginary;
+public:
+    void getdata() {
+        cout << "Enter the real number: ";
+        cin >> real;
+        cout << "Enter the imaginary number: ";
+        cin >> imaginary;
+    }
+    void showdata() {
+        cout << "The sum is " << real << " + " << imaginary << "i" << endl;
+    }
+
+    // Overloaded + operator for complex number addition using friend function
+    friend complex operator+(complex a, complex b) {
+        complex temp;
+        temp.real = a.real + b.real;
+        temp.imaginary = a.imaginary + b.imaginary;
+        return temp;
+    }
 };
-int main(){
-    str a,b,c;
+
+int main() {
+    complex a, b, c;
     a.getdata();
     b.getdata();
-    c=a+b;
-    c.show();
+
+    // Use the overloaded + operator to add a and b, and store the result in c
+    c = a + b;
+
+    c.showdata();
 }
