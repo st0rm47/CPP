3c11b146c00caf636d23afbe2011c6dc2ae6c780
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-27 21:24:04 +0545

    Lab report 5

--- labreport5.cpp
+++ labreport5.cpp
@@ -1,1 +1,12 @@
-/*WAP*/
+/*wap converting  basic datatype  to basic datatype*/
+#include<iostream>
+using namespace std;
+void data(){
+    int x;
+   float y=2.09;
+   x=y;                                      //converted into int type
+   cout<<"the value of x is: "<<x<<endl;
+}
+int main(){
+    data();
+}
