7a4eeb18d32d2abe8380a9d6ac948aaa9da574fc
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-28 10:30:04 +0545

    Lab report 6

---
+++ labreport6.cpp
@@ -0,0 +1,7 @@
+/*Create two classes named Mammals and Marine Animal. Create another class named Blue Whale which inherits both the above classes.
+Noe, Create a function in each of these classes which prints "I am Mammal", "I am Marine animals" and "I belong to both the categories:
+Mammals as well as marine animals" respectively. Now,create an object for each of the above class and try calling
+(i) function of mammals by object of mammal
+(ii) function of marine animal by object of marine animals
+(iii) function of blue whale by the objects of blue whale
+(iv) function of each of its present by object of blue whale*/
