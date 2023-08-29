faf31a549f2c528eaebdf1d03a56e276ed54fdb3
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-08-29 10:59:04 +0545

    Lab report 5 updated

--- labreport6.cpp
+++ labreport6.cpp
@@ -42,3 +42,58 @@ int main(){
     bw.Mammals::display();        //calling function of mammals using bluewhale
     bw.Marine_Animal::display();       //calling function of marine animals using bluewhale
 }
+
+
+
+/*Make a class named Fruit with data member to calculate no. of fruits in a basket.
+Create two other classes named Apples and Mangoes to calculate number of apples and mangoes in basket.
+Print the number of fruits of each type and total no. of fruits in the basket.*/
+
+#include<iostream>
+using namespace std;
+
+class Fruit{
+    int numFruits;
+
+    public:
+    Fruit(int num){
+        numFruits= num;
+    }
+
+    int get_num() {
+        return numFruits;
+    }
+};
+
+class Mangoes: public Fruit{
+    int nummangoes;
+    public:
+    Mangoes(int num): Fruit(num){}
+    void print_mangoes(){
+        cout<<"Number of mangoes: "<<get_num()<<endl;
+    }
+};
+
+class Apples: public Fruit{
+    public:
+    Apples(int num): Fruit(num){}
+
+    void print_apples(){
+        cout<<"Number of apples: "<<get_num()<<endl;
+    }
+};
+
+int main (){
+    Mangoes mango_object(5); //5 mangoes
+    Apples apple_object(3); // 3 mangoes
+    
+    //output part
+    mango_object.print_mangoes();
+    apple_object.print_apples();
+    
+    //total number of fruits
+    int totalFruits= apple_object.get_num() + mango_object.get_num();
+    cout<<"Total number of fruits in basket is: "<<totalFruits<<endl;
+
+    return 0;
+}
