7b6c1a397b9327e1accea7b49c3703d4c55b5970
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-02 21:20:12 +0545

    Lab report 5

--- labreport5.cpp
+++ labreport5.cpp
@@ -1,112 +1,112 @@
-/*Conversion from basic data type to anoyher basic data type*/
-#include<iostream>
-using namespace std;
-int main(){
-    int x;
-    float y=23.4;
-    /*x=int(y);    //Explicit Conversion */
-    x=y;        //Implicit Conversion   //Converted into integer type
-   cout << "The value of x is: "<< x << endl;
-}
+// // // // /*Conversion from basic data type to anoyher basic data type*/
+// // // // #include<iostream>
+// // // // using namespace std;
+// // // // int main(){
+// // // //     int x;
+// // // //     float y=23.4;
+// // // //     /*x=int(y);    //Explicit Conversion */
+// // // //     x=y;        //Implicit Conversion   //Converted into integer type
+// // // //    cout << "The value of x is: "<< x << endl;
+// // // // }
 
 
-/*Conversion from basic data type to userdefined data type*/
-#include<iostream>
-using namespace std;
-class dist{
-private:
-    int meter;
-    int centimeter;
-public:
-    dist(){
-        meter=0;
-        centimeter=0;
-    }
-    dist(int m){
-        meter=m/100;
-        centimeter=m%100;
-    }
-    void display(){
-        cout << "Distance = " <<  meter << " meter and " << centimeter << " centimeters" << endl;
-    }
-};
-int main(){
-    dist d1;
-    int x=120;
-    d1=x;
-    d1.display();
-    return 0;
-}
+// // // /*Conversion from basic data type to userdefined data type*/
+// // // #include<iostream>
+// // // using namespace std;
+// // // class dist{
+// // // private:
+// // //     int meter;
+// // //     int centimeter;
+// // // public:
+// // //     dist(){
+// // //         meter=0;
+// // //         centimeter=0;
+// // //     }
+// // //     dist(int m){
+// // //         meter=m/100;
+// // //         centimeter=m%100;
+// // //     }
+// // //     void display(){
+// // //         cout << "Distance = " <<  meter << " meter and " << centimeter << " centimeters" << endl;
+// // //     }
+// // // };
+// // // int main(){
+// // //     dist d1;
+// // //     int x=120;
+// // //     d1=x;
+// // //     d1.display();
+// // //     return 0;
+// // // }
 
 
-/*Conversion from user-defined data type to basic data type*/
-#include<iostream>
-using namespace std;
-class dist{
-private:
-    int meter;
-    int centimeter;
-public:
-    dist(){
-        meter=0;
-        centimeter=0;
-    }
-    dist(int m,int cm){
-        meter=m;
-        centimeter=cm;
-    }
-    // Conversion operator to convert dist to int
-    operator int(){
-        return meter*100+centimeter;
-    }
-};
-int main(){
-    dist d1(1,30);
-    int x=d1;
-    cout << "Distance = " << x << " centimeters" << endl;
-}
+// // /*Conversion from user-defined data type to basic data type*/
+// // #include<iostream>
+// // using namespace std;
+// // class dist{
+// // private:
+// //     int meter;
+// //     int centimeter;
+// // public:
+// //     dist(){
+// //         meter=0;
+// //         centimeter=0;
+// //     }
+// //     dist(int m,int cm){
+// //         meter=m;
+// //         centimeter=cm;
+// //     }
+// //     // Conversion operator to convert dist to int
+// //     operator int(){
+// //         return meter*100+centimeter;
+// //     }
+// // };
+// // int main(){
+// //     dist d1(1,30);
+// //     int x=d1;
+// //     cout << "Distance = " << x << " centimeters" << endl;
+// // }
 
 
-/*Conversion from user defined data type to another user defined data type (source)*/
-#include<iostream>
-using namespace std;
-class fahrenheit{
-private:
-    float f;
-public:
-    fahrenheit(){
-        f=0;
-    }
-    fahrenheit(int x){
-        f=x;
-    }
-    void show(){
-        cout << "Temperature in Fahrenheit: " << f << endl;
-    }
-};
-class celsius{
-private:
-    float c;
-public:
-    celsius(int x){
-        c=x;
-    }
+// // /*Conversion from user defined data type to another user defined data type (source)*/
+// #include<iostream>
+// using namespace std;
+// class fahrenheit{
+// private:
+//     float f;
+// public:
+//     fahrenheit(){
+//         f=0;
+//     }
+//     fahrenheit(int x){
+//         f=x;
+//     }
+//     void show(){
+//         cout << "Temperature in Fahrenheit: " << f << endl;
+//     }
+// };
+// class celsius{
+// private:
+//     float c;
+// public:
+//     celsius(int x){
+//         c=x;
+//     }
 
-    //Operator function in Source class
-    operator fahrenheit (){
-       return fahrenheit((c*9/5) + 32);
-    }
-};
-int main(){
-    fahrenheit f;  
-    celsius c(37);
-    f=c;
-    f.show();
-    return 0;
-}
+//     //Operator function in Source class
+//     operator fahrenheit (){
+//        return fahrenheit((c*9/5) + 32);
+//     }
+// };
+// int main(){
+//     fahrenheit f;  
+//     celsius c(45);
+//     f=c;
+//     f.show();
+//     return 0;
+// }
 
 
-/*Conversion from user defined data type to another user defined data type (destination)*/
+// /*Conversion from user defined data type to another user defined data type (destination)*/
 #include<iostream>
 using namespace std;
 class fahrenheit{
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,50 @@
-4
+/*Make a class named Fruit with data member to calculate no. of fruits in a basket.
+// Create two other classes named Apples and Mangoes to calculate number of apples and mangoes in basket.
+// Print the number of fruits of each type and total no. of fruits in the basket.*/
+
+// #include<iostream>
+// using namespace std;
+// class Fruit{
+// private:
+//     int numFruits;
+
+// public:
+//     Fruit(int num){
+//         numFruits= num;
+//     }
+
+//     int get_num() {
+//         return numFruits;
+//     }
+// };
+// class Mangoes: public Fruit{
+// private:
+//     int nummangoes;
+// public:
+//     Mangoes(int num): Fruit(num){}
+//     void print_mangoes(){
+//         cout<<"Number of mangoes: " << get_num() << endl;
+//     }
+// };
+// class Apples: public Fruit{
+//     public:
+//     Apples(int num): Fruit(num){}
+
+//     void print_apples(){
+//         cout<<"Number of apples: " <<get_num() << endl;
+//     }
+// };
+// int main (){
+//     Mangoes mango_object(5); //5 mangoes
+//     Apples apple_object(3); // 3 mangoes
+    
+//     //output part
+//     mango_object.print_mangoes();
+//     apple_object.print_apples();
+    
+//     //total number of fruits
+//     int totalFruits= apple_object.get_num() + mango_object.get_num();
+//     cout<<"Total number of fruits in basket is: " << totalFruits << endl;
+
+//     return 0;
+// }
