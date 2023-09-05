b7e616313baa58cc4eb47c0c71b0e7364fee402d
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-05 10:27:43 +0545

    Lab report 6

---
+++ Labreport6.cpp
@@ -0,0 +1,9 @@
+/*1. WAP to resolve ambiguity in multiple inheritance.*/
+
+/*2. WAP to illustrate concept of function overloading.*/
+
+/*3. WAP to abstract class using concept of pure virtual function.*/
+
+/*4. WAP to illustrate the use of virtual function.*/
+
+/*5. WAP to solve ambiguity problem in multipath inheritance with the concept of virtual class.*/
