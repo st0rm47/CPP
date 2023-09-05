25681b230deeb4c63dfb806945adcd832d0c49d5
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-05 11:53:14 +0545

    Lab report 6

--- Labreport6.cpp
+++ Labreport6.cpp
@@ -1,4 +1,36 @@
-/*1. WAP to resolve ambiguity in multiple inheritance.*/
+// /*1. WAP to resolve ambiguity in multiple inheritance.*/
+// #include<iostream>
+// using namespace std;
+// class A {
+// 	public:
+// 	void show() {
+// 		cout << " I am in class A" << endl;
+// 	}
+// };
+// class B {
+// 	public:
+// 	void show() {
+// 		cout << " I am in class B" << endl;
+// 	}
+// };
+// class C: public A, public B {
+// public:
+//     // Member function in class C, which overrides the show() method
+//     void show() {
+//         // Call the show() method of class A using A::show()
+//         A::show();
+//         // Call the show() method of class B using B::show()
+//         B::show();
+//     }
+// };
+// int main() {
+// 	C obj;
+//     obj.show();
+// 	return 0;
+// }
+
+
+/*2. WAP to illustrate concept of function overriading.*/
 #include<iostream>
 using namespace std;
 class A {
@@ -7,30 +39,18 @@ class A {
 		cout << " I am in class A" << endl;
 	}
 };
-class B {
+class B: public A{
 	public:
 	void show() {
 		cout << " I am in class B" << endl;
 	}
 };
-class C: public A, public B {
-public:
-    // Member function in class C, which overrides the show() method
-    void show() {
-        // Call the show() method of class A using A::show()
-        A::show();
-        // Call the show() method of class B using B::show()
-        B::show();
-    }
-};
 int main() {
-	C obj;
-    obj.show();
+	B b;
+    A *ptr=&b;
+    ptr->show();
 	return 0;
 }
-
-/*2. WAP to illustrate concept of function overloading.*/
-
 /*3. WAP to abstract class using concept of pure virtual function.*/
 
 /*4. WAP to illustrate the use of virtual function.*/
@@ -38,3 +58,8 @@ int main() {
 /*5. WAP to solve ambiguity problem in multipath inheritance with the concept of virtual class.*/
 // C++ program to show inheritance ambiguity
 
+#include<iostream>
+using namespace std;
+class A{
+    
+}
