7aaa09106a40c1ec8897e28e7c2cbd1a910d531c
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-05 10:43:37 +0545

    Lab report 6

--- Labreport6.cpp
+++ Labreport6.cpp
@@ -1,4 +1,33 @@
 /*1. WAP to resolve ambiguity in multiple inheritance.*/
+#include<iostream>
+using namespace std;
+class A {
+	public:
+	void show() {
+		cout << " I am in class A" << endl;
+	}
+};
+class B {
+	public:
+	void show() {
+		cout << " I am in class B" << endl;
+	}
+};
+class C: public A, public B {
+public:
+    // Member function in class C, which overrides the show() method
+    void show() {
+        // Call the show() method of class A using A::show()
+        A::show();
+        // Call the show() method of class B using B::show()
+        B::show();
+    }
+};
+int main() {
+	C obj;
+    obj.show();
+	return 0;
+}
 
 /*2. WAP to illustrate concept of function overloading.*/
 
@@ -7,3 +36,5 @@
 /*4. WAP to illustrate the use of virtual function.*/
 
 /*5. WAP to solve ambiguity problem in multipath inheritance with the concept of virtual class.*/
+// C++ program to show inheritance ambiguity
+
