55084c4d0903278934bf06656a8a51d8cbfcb2a0
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-06 18:57:32 +0545

    Lab report 6

--- Labreport6.cpp
+++ Labreport6.cpp
@@ -31,26 +31,27 @@ int main() {
 
 
 // /*2. WAP to illustrate concept of function overriading.*/
-// #include<iostream>
-// using namespace std;
-// class A {
-// 	public:
-// 	void show() {
-// 		cout << " I am in class A" << endl;
-// 	}
-// };
-// class B: public A{
-// 	public:
-// 	void show() {
-// 		cout << " I am in class B" << endl;
-// 	}
-// };
-// int main() {
-// 	B b;
-//     A *ptr=&b;
-//     ptr->show();
-// 	return 0;
-// }
+#include<iostream>
+using namespace std;
+class College{
+    public:
+    void show(){
+        cout << "This is MBM College" << endl;
+    }
+};
+class CSIT: public College{
+    public:
+    void show(){   
+        cout<<"This is CSIT stream"<<endl;
+    }
+};
+
+int main(){
+    CSIT c;
+    c.show(); //Calling member function from class CSIT
+    c.College::show(); //Accessing class College through class CSIT
+    return 0;
+}
 /*3. WAP to abstract class using concept of pure virtual function.*/
 
 /*4. WAP to illustrate the use of virtual function.*/
