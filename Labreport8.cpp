ed2fc2d3e5367ec6944ff9ab60da00b433348fa1
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-12 11:19:10 +0545

    Lab report 8

---
+++ Labreport8.cpp
@@ -0,0 +1,48 @@
+/*1.WAP to find the largest among two numbers using function template.*/
+#include<iostream>
+using namespace std;
+
+template<class A>
+A maxnum(A a,A b){
+    A result;
+    result = (a>b)?a:b;
+    return result;
+}
+int main(){
+    int a=5,b=10,x;
+    float c=2.9,d=3.9,y;
+
+    x=maxnum(a,b);
+    y=maxnum(c,d);
+
+    cout << "Largest among 2 integers : " << x << endl;
+    cout << "Largest among 2 float : " << y << endl;
+    return 0;
+}
+
+/*2.WAP to swap data using template function.*/
+#include<iostream>
+using namespace std;
+
+template<class A>
+A maxnum(A a,A b){
+    A result;
+    result = (a>b)?a:b;
+    return result;
+}
+int main(){
+    int a=5,b=10,x;
+    float c=2.9,d=3.9,y;
+
+    x=maxnum(a,b);
+    y=maxnum(c,d);
+
+    cout << "Largest among 2 integers : " << x << endl;
+    cout << "Largest among 2 float : " << y << endl;
+    return 0;
+}
+/*3.WAP to build simple calculator using class template.*/
+
+/*4.WAP to find sum of numbers using function template overloading.*/
+
+/*5.WAP to find sum of array using function template.*/
