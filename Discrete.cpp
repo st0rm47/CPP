a83e77f67133567f38260e9202f1f9c271691811
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-12 10:25:44 +0545

    DS LAB

--- Discrete.cpp
+++ Discrete.cpp
@@ -1,51 +1,51 @@
-#include <iostream>
-#include <set>
-using namespace std;
-int main() {
-    set<int> set1, set2, unionSet;
-    int n1, n2, element;
-    cout << "\t\t========================" << endl;
-    cout << "\t\t   Union of Two Sets    " << endl;
-    cout << "\t\t========================" << endl;
+// #include <iostream>
+// #include <set>
+// using namespace std;
+// int main() {
+//     set<int> set1, set2, unionSet;
+//     int n1, n2, element;
+//     cout << "\t\t========================" << endl;
+//     cout << "\t\t   Union of Two Sets    " << endl;
+//     cout << "\t\t========================" << endl;
 
-    // Input for the first set
-    cout << "Enter the number of elements in the first set: ";
-    cin >> n1;
-    cout << "Enter the elements of the first set, separated by spaces: ";
-    for (int i = 0; i < n1; ++i) {
-        cin >> element;
-        set1.insert(element);
-    }
+//     // Input for the first set
+//     cout << "Enter the number of elements in the first set: ";
+//     cin >> n1;
+//     cout << "Enter the elements of the first set, separated by spaces: ";
+//     for (int i = 0; i < n1; ++i) {
+//         cin >> element;
+//         set1.insert(element);
+//     }
 
-    // Input for the second set
-    cout << "Enter the number of elements in the second set: ";
-    cin >> n2;
-    cout << "Enter the elements of the second set, separated by spaces: ";
-    for (int i = 0; i < n2; ++i) {
-        cin >> element;
-        set2.insert(element);
-    }
+//     // Input for the second set
+//     cout << "Enter the number of elements in the second set: ";
+//     cin >> n2;
+//     cout << "Enter the elements of the second set, separated by spaces: ";
+//     for (int i = 0; i < n2; ++i) {
+//         cin >> element;
+//         set2.insert(element);
+//     }
 
-    // Compute the union of the two sets
-    for (int x : set1) {
-        unionSet.insert(x);
-    }
-    for (int x : set2) {
-        unionSet.insert(x);
-    }
+//     // Compute the union of the two sets
+//     for (int x : set1) {
+//         unionSet.insert(x);
+//     }
+//     for (int x : set2) {
+//         unionSet.insert(x);
+//     }
 
-    // Display the union set
-    cout << "Union of the two sets: {";
-    int first = 1;
-    for (int x : unionSet) {
-        if (x > 0) {
-            cout << (!first ? "," : "") << x;
-            first = 0;
-        }
-    }
-    cout << "}"  << endl;
-    return 0;
-}
+//     // Display the union set
+//     cout << "Union of the two sets: {";
+//     int first = 1;
+//     for (int x : unionSet) {
+//         if (x > 0) {
+//             cout << (!first ? "," : "") << x;
+//             first = 0;
+//         }
+//     }
+//     cout << "}"  << endl;
+//     return 0;
+// }
 
 
 // #include <iostream>
@@ -98,3 +98,55 @@ int main() {
 //     cout << "}"  << endl;
 //     return 0;
 // }
+
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    set<int> set1, set2, differenceSet; 
+
+    int n1, n2, element;
+
+    cout << "\t\t============================" << endl;
+    cout << "\t\t   Difference of Two Sets   " << endl;
+    cout << "\t\t============================" << endl;
+
+    // Input for the first set
+    cout << "Enter the number of elements in the first set: ";
+    cin >> n1;
+    cout << "Enter the elements of the first set, separated by spaces: ";
+    for (int i = 0; i < n1; ++i) {
+        cin >> element;
+        set1.insert(element);
+    }
+
+    // Input for the second set
+    cout << "Enter the number of elements in the second set: ";
+    cin >> n2;
+    cout << "Enter the elements of the second set, separated by spaces: ";
+    for (int i = 0; i < n2; ++i) {
+        cin >> element;
+        set2.insert(element);
+    }
+
+    // Compute the difference of the two sets (set1 - set2)
+    for (int x : set1) {
+        if (set2.find(x) == set2.end()) {
+            differenceSet.insert(x);
+        }
+    }
+
+    // Display the difference set
+    cout << "Difference of the two sets (set1 - set2): {";
+    int first = 1;
+    for (int x : differenceSet) {
+        if (x > 0) {
+            cout << (!first ? "," : "") << x;
+            first = 0;
+        }
+    }
+    cout << "}"  << endl;
+    return 0;
+}
