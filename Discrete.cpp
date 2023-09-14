b07c9e894500b5ae671247cc4c2f0428f62bdff2
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-14 14:04:00 +0545

    Discrete Structure

--- Discrete.cpp
+++ Discrete.cpp
@@ -1,220 +1,188 @@
+// /*Union Set*/
 // #include <iostream>
-// #include <set>
 // using namespace std;
-// int main() {
-//     set<int> set1, set2, unionSet;
+
+// int main() 
+// {
+//     const int maxSize = 100;
+//     int set_A[maxSize], set_B[maxSize], unionSet[maxSize];
 //     int n1, n2, element;
+//     int unionSize = 0;
+
 //     cout << "\t\t========================" << endl;
 //     cout << "\t\t   Union of Two Sets    " << endl;
 //     cout << "\t\t========================" << endl;
 
 //     // Input for the first set
-//     cout << "Enter the number of elements in the first set: ";
+//     cout << "Enter the number of elements in Set A: ";
 //     cin >> n1;
-//     cout << "Enter the elements of the first set, separated by spaces: ";
-//     for (int i = 0; i < n1; ++i) {
+//     cout << "Enter the elements of Set A, separated by spaces: ";
+//     for (int i = 0; i < n1; ++i) 
+//     {
 //         cin >> element;
-//         set1.insert(element);
+//         set_A[i] = element;
 //     }
 
 //     // Input for the second set
-//     cout << "Enter the number of elements in the second set: ";
+//     cout << "Enter the number of elements in Set B: ";
 //     cin >> n2;
-//     cout << "Enter the elements of the second set, separated by spaces: ";
-//     for (int i = 0; i < n2; ++i) {
+//     cout << "Enter the elements of Set B, separated by spaces: ";
+//     for (int i = 0; i < n2; ++i) 
+//     {
 //         cin >> element;
-//         set2.insert(element);
+//         set_B[i] = element;
 //     }
 
+
+
 //     // Compute the union of the two sets
-//     for (int x : set1) {
-//         unionSet.insert(x);
-//     }
-//     for (int x : set2) {
-//         unionSet.insert(x);
+//     for (int i = 0; i < n1; ++i)
+//         unionSet[unionSize++] = set_A[i];
+
+//     for (int i = 0; i < n2; ++i) 
+//     {
+//         int isDuplicate = 0;
+//         for (int j = 0; j < unionSize; ++j) 
+//         {
+//             if (set_B[i] == unionSet[j]) 
+//             {
+//                 isDuplicate = 1;
+//                 break;
+//             }
+//         }
+//         if (!isDuplicate)
+//             unionSet[unionSize++] = set_B[i];
 //     }
 
 //     // Display the union set
 //     cout << "Union of the two sets: {";
 //     int first = 1;
-//     for (int x : unionSet) {
-//         if (x > 0) {
-//             cout << (!first ? "," : "") << x;
-//             first = 0;
-//         }
+//     for (int i = 0; i < unionSize; ++i) 
+//     {
+//         cout << (first ? "" : ",") << unionSet[i];
+//         first = 0;
 //     }
-//     cout << "}"  << endl;
+//     cout << "}" << endl;
 //     return 0;
 // }
 
 
+
+// /*Intersection Set*/
 // #include <iostream>
-// #include <set>
 // using namespace std;
 
-// int main() {
-//     set<int> set1, set2, intersectionSet; // Define an intersectionSet.
-
+// int main() 
+// {
+//     const int maxSize = 100;
+//     int set_A[maxSize], set_B[maxSize], intersectionSet[maxSize];
 //     int n1, n2, element;
+//     int intersectionSize = 0;
 
-//     cout << "\t\t============================" << endl;
-//     cout << "\t\t  Intersection of Two Sets  " << endl;
-//     cout << "\t\t============================" << endl;
+//     cout << "\t\t==========================" << endl;
+//     cout << "\t\t Intersection of Two Sets " << endl;
+//     cout << "\t\t==========================" << endl;
 
 //     // Input for the first set
-//     cout << "Enter the number of elements in the first set: ";
+//     cout << "Enter the number of elements in Set A: ";
 //     cin >> n1;
-//     cout << "Enter the elements of the first set, separated by spaces: ";
-//     for (int i = 0; i < n1; ++i) {
+//     cout << "Enter the elements of Set A, separated by spaces: ";
+//     for (int i = 0; i < n1; ++i) 
+//     {
 //         cin >> element;
-//         set1.insert(element);
+//         set_A[i] = element;
 //     }
 
 //     // Input for the second set
-//     cout << "Enter the number of elements in the second set: ";
+//     cout << "Enter the number of elements in Set B: ";
 //     cin >> n2;
-//     cout << "Enter the elements of the second set, separated by spaces: ";
-//     for (int i = 0; i < n2; ++i) {
+//     cout << "Enter the elements of Set B, separated by spaces: ";
+//     for (int i = 0; i < n2; ++i) 
+//     {
 //         cin >> element;
-//         set2.insert(element);
+//         set_B[i] = element;
 //     }
 
+
+
 //     // Compute the intersection of the two sets
-//     for (int x : set1) {
-//         if (set2.find(x) != set2.end()) {
-//             intersectionSet.insert(x);
+//     for (int i = 0; i < n1; ++i) 
+//     {
+//         for (int j = 0; j < n2; ++j) 
+//         {
+//             if (set_A[i] == set_B[j]) 
+//             {
+//                 intersectionSet[intersectionSize++] = set_A[i];
+//                 break;
+//             }
 //         }
 //     }
 
 //     // Display the intersection set
 //     cout << "Intersection of the two sets: {";
 //     int first = 1;
-//     for (int x : intersectionSet) {
-//         if (x > 0) {
-//             cout << (!first ? "," : "") << x;
-//             first = 0;
-//         }
+//     for (int i = 0; i < intersectionSize; ++i) 
+//     {
+//         cout << (first ? "" : ",") << intersectionSet[i];
+//         first = 0;
 //     }
-//     cout << "}"  << endl;
+//     cout << "}" << endl;
 //     return 0;
 // }
 
 
-// #include <iostream>
-// #include <set>
-// using namespace std;
-
-// int main() {
-//     set<int> set1, set2, differenceSet; 
-
-//     int n1, n2, element;
-
-//     cout << "\t\t============================" << endl;
-//     cout << "\t\t   Difference of Two Sets   " << endl;
-//     cout << "\t\t============================" << endl;
-
-//     // Input for the first set
-//     cout << "Enter the number of elements in the first set: ";
-//     cin >> n1;
-//     cout << "Enter the elements of the first set, separated by spaces: ";
-//     for (int i = 0; i < n1; ++i) {
-//         cin >> element;
-//         set1.insert(element);
-//     }
-
-//     // Input for the second set
-//     cout << "Enter the number of elements in the second set: ";
-//     cin >> n2;
-//     cout << "Enter the elements of the second set, separated by spaces: ";
-//     for (int i = 0; i < n2; ++i) {
-//         cin >> element;
-//         set2.insert(element);
-//     }
-
-//     // Compute the difference of the two sets (set1 - set2)
-//     for (int x : set1) {
-//         if (set2.find(x) == set2.end()) {
-//             differenceSet.insert(x);
-//         }
-//     }
-
-//     // Display the difference set
-//     cout << "Difference of the two sets (set1 - set2): {";
-//     int first = 1;
-//     for (int x : differenceSet) {
-//         if (x > 0) {
-//             cout << (!first ? "," : "") << x;
-//             first = 0;
-//         }
-//     }
-//     cout << "}"  << endl;
-//     return 0;
-// }
-
-
+/*Difference Set*/
 #include <iostream>
 using namespace std;
-int main() 
-{
-    const int maxSize = 100;
-    int set1[maxSize], set2[maxSize], unionSet[maxSize];
-    int n1, n2, element;
-    int unionSize = 0;
 
-    cout << "\t\t========================" << endl;
-    cout << "\t\t   Union of Two Sets    " << endl;
-    cout << "\t\t========================" << endl;
+int main() {
+    const int maxSize = 100;
+    int set_A[maxSize], set_B[maxSize], differenceSet[maxSize];
+    int n1, n2, element;
+    int differenceSize = 0;
+
+    cout << "\t\t======================" << endl;
+    cout << "\t\t Difference of Two Sets" << endl;
+    cout << "\t\t======================" << endl;
 
     // Input for the first set
-    cout << "Enter the number of elements in the first set: ";
+    cout << "Enter the number of elements in Set A: ";
     cin >> n1;
-    cout << "Enter the elements of the first set, separated by spaces: ";
-    for (int i = 0; i < n1; ++i) 
-    {
+    cout << "Enter the elements of Set A, separated by spaces: ";
+    for (int i = 0; i < n1; ++i) {
         cin >> element;
-        set1[i] = element;
+        set_A[i] = element;
     }
 
     // Input for the second set
-    cout << "Enter the number of elements in the second set: ";
+    cout << "Enter the number of elements in Set B: ";
     cin >> n2;
-    cout << "Enter the elements of the second set, separated by spaces: ";
-    for (int i = 0; i < n2; ++i) 
-    {
+    cout << "Enter the elements of Set B, separated by spaces: ";
+    for (int i = 0; i < n2; ++i) {
         cin >> element;
-        set2[i] = element;
+        set_B[i] = element;
     }
 
-    // Compute the union of the two sets
-    for (int i = 0; i < n1; ++i)
-        unionSet[unionSize++] = set1[i];
-
-    for (int i = 0; i < n2; ++i) 
-    {
-        int isDuplicate = 0;
-        for (int j = 0; j < unionSize; ++j) 
-        {
-            if (set2[i] == unionSet[j]) 
-            {
-                isDuplicate = 1;
+    // Compute the difference between Set A and Set B
+    for (int i = 0; i < n1; ++i) {
+        int isDifferent = 1;  // Assume the element is different initially
+        for (int j = 0; j < n2; ++j) {
+            if (set_A[i] == set_B[j]) {
+                isDifferent = 0;  // Element found in Set B, not different
                 break;
             }
         }
-        if (!isDuplicate) {
-            unionSet[unionSize++] = set2[i];
+        if (isDifferent) {
+            differenceSet[differenceSize++] = set_A[i];
         }
     }
 
-    // Display the union set
-    cout << "Union of the two sets: {";
+    // Display the difference set
+    cout << "Difference of Set A - Set B: {";
     int first = 1;
-    for (int i = 0; i < unionSize; ++i) 
-    {
-        if (unionSet[i] > 0) 
-        {
-            cout << (!first ? "," : "") << unionSet[i];
-            first = 0;
-        }
+    for (int i = 0; i < differenceSize; ++i) {
+        cout << (first ? "" : ",") << differenceSet[i];
+        first = 0;
     }
     cout << "}" << endl;
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,5 +1,1 @@
-for (int x : set1) {
-        if (set2.find(x) != set2.end()) {
-            intersectionSet.insert(x);
-        }
-    }
+<<
