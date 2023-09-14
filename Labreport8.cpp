156b00f1ec02931549fd8bc00c7b9d1288b52753
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-13 23:04:19 -0600

    Lab report 8

--- Labreport8.cpp
+++ Labreport8.cpp
@@ -21,26 +21,7 @@ int main(){
 }
 
 /*2.WAP to swap data using template function.*/
-#include<iostream>
-using namespace std;
 
-template<typename A,typename B>
-A swap(A a,B b){
-    A result;
-    result = (a>b)?a:b;
-    return result;
-}
-int main(){
-    int a=5,b=10,x;
-    float c=2.9,d=3.9,y;
-
-    x=maxnum(a,b);
-    y=maxnum(c,d);
-
-    cout << "Largest among 2 integers : " << x << endl;
-    cout << "Largest among 2 float : " << y << endl;
-    return 0;
-}
 /*3.WAP to build simple calculator using class template.*/
 
 /*4.WAP to find sum of numbers using function template overloading.*/
