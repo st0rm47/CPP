e9b56037777d8fe9bfc01d5766d2c1fb52daf1ee
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-15 18:01:58 +0545

    Type Conversion

---
+++ Type_Conversion.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+    //Namespace
+    namespace first{
+        int var=5;
+    }
+    namespace second{
+        int var=6;
+    }
+int main(){
+    //Implicit Type Conversion
+    float x=3.45f;
+    int y;
+    y=x;
+    cout << y << endl;
+
+    //Explicit Type Conversion
+    // float x=3.45;
+    // int y;
+    // y=int(x);
+    // cout << y;
+
+    //Dynamic Memory Allocation
+    // int *a=new int(20);
+    // delete a;
+
+    //Enumeration
+    enum {Sun,Mon,Tue,Wed,Thu,Fri,Sat};
+
+    //Namespacce
+    cout << first :: var << endl;
+    cout << second :: var << endl;
+    
+}
