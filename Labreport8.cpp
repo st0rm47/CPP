7d0dbdc3a77a9dee290faf02eb3d22f40de906dd
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-16 19:56:31 +0545

    Lab report 8

--- Labreport8.cpp
+++ Labreport8.cpp
@@ -1,28 +1,71 @@
 /*1.WAP to find the largest among two numbers using function template.*/
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-template<class A>
-A maxnum(A a, A b){
-    A result;
-    result = (a>b)?a:b;
-    return result;
-}
-int main(){
-    int a=5,b=10,x;
-    float c=2.9,d=3.9,y;
+// template<class A>
+// A maxnum(A a, A b){
+//     A result;
+//     result = (a>b)?a:b;
+//     return result;
+// }
+// int main(){
+//     int a=5,b=10,x;
+//     float c=2.9,d=3.9,y;
 
-    x=maxnum(a,b);
-    y=maxnum(c,d);
+//     x=maxnum(a,b);
+//     y=maxnum(c,d);
 
-    cout << "Largest among 2 integers : " << x << endl;
-    cout << "Largest among 2 float : " << y << endl;
-    return 0;
-}
+//     cout << "Largest among 2 integers : " << x << endl;
+//     cout << "Largest among 2 float : " << y << endl;
+//     return 0;
+// }
 
 /*2.WAP to swap data using template function.*/
 
 /*3.WAP to build simple calculator using class template.*/
+#include<iostream>
+using namespace std;
+template <class A>
+class Calculator{
+    private:
+        A a,b;
+    public:
+        Calculator (A x, A y){
+            a=x;
+            b=y;
+        }
+        A add(){
+            return a+b;
+        }
+        A sub(){
+            return a-b;
+        }
+        A mul(){
+            return a*b;
+        }
+        A div(){
+            return a/b;
+        }
+        void display();
+};
+template <class A>
+void Calculator <A> :: display(){
+    cout << "Numbers: " << a << " and " << b << endl;
+    cout << a << " + " << b << " = " << add() << endl;
+    cout << a << " - " << b << " = " << sub() << endl;
+    cout << a << " * " << b << " = " << mul() << endl;
+    cout << a << " / " << b << " = " << div() << endl;
+}
+int main(){
+    Calculator <int> c1(2,4);
+    Calculator <float> c2(3.5,4.5);
+
+    cout << "Caclulator for Integers: " << endl;
+    c1.display();
+
+    cout << "Caclulator for Float: " << endl;
+    c2.display();
+}
 
 /*4.WAP to find sum of numbers using function template overloading.*/
 
