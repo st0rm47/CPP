5582f0f766323c382bf987ecb041c9c59f426cb3
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-16 10:43:47 +0545

    Friend Class Function

---
+++ 12_FriendFunction.cpp
@@ -0,0 +1,59 @@
+/* Friend Function */
+#include<iostream>
+using namespace std;
+class friendfun{
+    int a,b;
+    public:
+    void getdata()
+    {
+        cout <<"Enter values of a and b: ";
+        cin >> a >> b;
+    }
+    friend void show (friendfun f);
+};
+void show(friendfun f){
+    cout << f.a << endl << f.b;
+}
+int main (){
+    friendfun f;
+    f.getdata();
+    show(f);
+    return 0;
+}
+
+
+/* Friend Function acting as a bridge */
+#include<iostream>
+using namespace std;
+class two;      //Second class declaration
+class one{
+    int x,y;    
+    public:
+        void getdata(){           
+           cout << "Enter values of x and y for class one : ";
+            cin >> x >> y;
+        }
+        friend int display(one a,two b );   //Friend function declaration with keyword friend and argument as object  
+};
+class two{
+    int x,y;
+    public:
+        void getdata(){            
+           cout << "Enter values of x and y for class two :";
+            cin >> x >> y;
+        }
+        friend int display(one a,two b); 
+};
+int display(one a,two b){     
+    return a.x+b.x;
+    return a.y+b.y;;
+}
+int main(){
+    one a;     
+    two b;
+    a.getdata();
+    b.getdata();
+    cout << display(a,b) << endl;
+    cout << display(a,b);
+    return 0;
+}
