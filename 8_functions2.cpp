e4eaa1f487fd4417190ef62017718e5d557c0a24
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-16 10:43:14 +0545

    Function

---
+++ 8_functions2.cpp
@@ -0,0 +1,33 @@
+/*-->Default Argument and Macros*/
+#include<iostream>
+using namespace std;
+#define mul(a,b)a*b             // Macro Definition
+inline int sum(int x=5,int y=6){       // Default Arguments
+    return x+y;
+}
+int main(){
+    cout << mul(3,4) << endl;           // Macro Call
+    cout << sum(3,2);           // Function Call
+    return 0;
+}
+
+//Overloading of Functions
+#include<iostream>
+using namespace std;
+void mul(int x, int y) {
+    cout << x*y << endl;
+}
+void mul(double x, double y, double z) {
+    cout << x*y << endl;
+}
+void mul(float x, float y){
+    cout << x*y << endl;
+}
+int main(){
+    mul(3,4);
+    mul(4.2,3.2,2.02);
+    mul(3.2f,5.2f);
+}
+
+
+
