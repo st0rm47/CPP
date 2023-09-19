97afae12d47adfe395e0f6e252daf3726db5578b
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-19 13:19:30 +0545

    Lab

--- lab.cpp
+++ lab.cpp
@@ -1,33 +1,33 @@
-#include<iostream>
-using namespace std;
-class bio{  //class
-    private:
-    int age;
-    string name;
-    float salary;
-    public:
-        void getdata(){ // Member function inside a class
-            cout << "Enter your name: ";
-            cin >> name;
-            cout << "Enter your age: ";
-            cin >> age;
-            cout << "Enter your salary: ";
-            cin >> salary;
-            cout << endl;
-        }
+// #include<iostream>
+// using namespace std;
+// class bio{  //class
+//     private:
+//     int age;
+//     string name;
+//     float salary;
+//     public:
+//         void getdata(){ // Member function inside a class
+//             cout << "Enter your name: ";
+//             cin >> name;
+//             cout << "Enter your age: ";
+//             cin >> age;
+//             cout << "Enter your salary: ";
+//             cin >> salary;
+//             cout << endl;
+//         }
     
-    void data();    //Members of the class
-    void ssama();   //Members of the class
-};
-void bio :: ssama(){    // Member function outside a class
-    cout << "Salary = " << salary << endl;
+//     void data();    //Members of the class
+//     void ssama();   //Members of the class
+// };
+// void bio :: ssama(){    // Member function outside a class
+//     cout << "Salary = " << salary << endl;
 
-}
-void bio :: data(){     // Member function outside a class
-    cout << "Name = " << name << endl;
-    cout << "Age = " << age << endl;
-    cout << endl;
-}
+// }
+// void bio :: data(){     // Member function outside a class
+//     cout << "Name = " << name << endl;
+//     cout << "Age = " << age << endl;
+//     cout << endl;
+// }
 
 // int main(){
 // bio mydata[10]; //object using array
@@ -43,3 +43,34 @@ void bio :: data(){     // Member function outside a class
 // }
 
 // }
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    char op;
+    cout << "Enter the operator (+,-,*,/): ";
+    cin >> op;
+    cout << "Enter 2 numbers: ";
+    cin >> a >> b;
+
+    switch(op){
+    case '+':
+        cout << a << " + " << b << " = " << a + b << endl;
+        break;
+    case '-':
+        cout << a << " - " << b << " = " << a - b << endl;
+        break;
+    case '*':
+        cout << a << " * " << b << " = " << a * b << endl;
+        break;
+    case '/':
+            cout << a << " / " << b << " = " << a / b << endl;
+        break;
+    default:
+        cout << "Invalid Operation" << endl;
+        break;
+    }
+       return 0;
+}
