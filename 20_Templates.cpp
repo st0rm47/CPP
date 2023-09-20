579ffbdc70e20aa1c2ddf7d9df9382850321a2ec
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-20 07:41:02 +0545

    Templates

--- 20_Templates.cpp
+++ 20_Templates.cpp
@@ -1,92 +1,92 @@
-// /*Function Templates*/
-// /*Comparision betweeen same data type*/
-// // #include<iostream>
-// // using namespace std;
-// // template<class A>
-// // A maxnum(A a, A b){
-// //     if(a>b){
-// //         return a;
-// //     }
-// //     else
-// //         return b;
-// // }
-// // int main(){
-// //     int a=5,b=10,x;
-// //     float c=2.9,d=3.9,y;
+/*Function Templates*/
+/*Comparision betweeen same data type*/
+#include<iostream>
+using namespace std;
+template<class A>
+A maxnum(A a, A b){
+    if(a>b){
+        return a;
+    }
+    else
+        return b;
+}
+int main(){
+    int a=5,b=10,x;
+    float c=2.9,d=3.9,y;
 
-// //     x=maxnum(a,b);
-// //     y=maxnum(c,d);
+    x=maxnum(a,b);
+    y=maxnum(c,d);
 
-// //     cout << "Largest among 2 integers : " << x << endl;
-// //     cout << "Largest among 2 float : " << y << endl;
-// //     return 0;
-// // }
+    cout << "Largest among 2 integers : " << x << endl;
+    cout << "Largest among 2 float : " << y << endl;
+    return 0;
+}
 
-// /*Comparision betweeen different data type*/
-// // #include<iostream>
-// // using namespace std;
-// // template<class A,class B>
-// // A maxnum(A a, B b){
-// //     if(a>b){
-// //         return a;
-// //     }
-// //     else
-// //         return b;
-// // }
-// // int main(){
-// //     int a=5,x;
-// //     float b=2.9,y;
+/*Comparision betweeen different data type*/
+#include<iostream>
+using namespace std;
+template<class A,class B>
+A maxnum(A a, B b){
+    if(a>b){
+        return a;
+    }
+    else
+        return b;
+}
+int main(){
+    int a=5,x;
+    float b=2.9,y;
 
-// //     x=maxnum(a,b);
+    x=maxnum(a,b);
 
-// //     cout << "Largest among 2 numbers : " << x << endl;
-// //     return 0;
-// // }
+    cout << "Largest among 2 numbers : " << x << endl;
+    return 0;
+}
 
-// /*Overloading of Function Templates*/
-// #include<iostream>
-// using namespace std;
-// template<class A>
-// void print(A a){
-//     cout << a << endl;
-// }
-// template<class A, class B>
-// void print(A a, B b){
-//     cout << a << b << endl;
-// }
-// int main(){
-//     int a=5;
-//     print(a);
-//     float b = 4.5;
-//     print(a,b);
-// }
+/*Overloading of Function Templates*/
+#include<iostream>
+using namespace std;
+template<class A>
+void print(A a){
+    cout << a << endl;
+}
+template<class A, class B>
+void print(A a, B b){
+    cout << a << b << endl;
+}
+int main(){
+    int a=5;
+    print(a);
+    float b = 4.5;
+    print(a,b);
+}
 
 /*Class Templates*/
-// #include<iostream>
-// using namespace std;
-// template<class Apple>
-// class my{
-//     Apple a,b;
-//     public:
-//     my(Apple x, Apple y){
-//         a=x;
-//         b=y;
-//     }
-//     Apple maxnum(){
-//         if(a>b){
-//             return a;
-//         }
-//         else
-//             return b;
-//     }
-// };
-// int main(){
-//     my <int> m1(10,75);
-//     cout << "Largest Integer = " << m1.maxnum() << endl;
+#include<iostream>
+using namespace std;
+template<class Apple>
+class my{
+    Apple a,b;
+    public:
+    my(Apple x, Apple y){
+        a=x;
+        b=y;
+    }
+    Apple maxnum(){
+        if(a>b){
+            return a;
+        }
+        else
+            return b;
+    }
+};
+int main(){
+    my <int> m1(10,75);
+    cout << "Largest Integer = " << m1.maxnum() << endl;
 
-//     my <float> m2(20.5,30.6);
-//     cout << "Largest Float = " << m2.maxnum() << endl;
-// }
+    my <float> m2(20.5,30.6);
+    cout << "Largest Float = " << m2.maxnum() << endl;
+}
 
 
 #include<iostream>
