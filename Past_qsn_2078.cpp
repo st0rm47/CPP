a1c89013fe7c37964a04b682d83ed23b2ad5822a
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-20 15:08:13 +0545

    Past Question

---
+++ Past_qsn_2078.cpp
@@ -0,0 +1,152 @@
+/* 2078 */
+
+/*1.*/
+#include <iostream>
+using namespace std;
+class Account {
+private:
+    int accNo;
+    double balance;
+    static double minBalance; // Static member for minimum balance
+public:
+    Account() {
+        accNo = 0;
+        balance = 0.0;
+    }
+    void readData() {
+        cout << "Enter Account Number: ";
+        cin >> accNo;
+        cout << "Enter Balance: ";
+        cin >> balance;
+    }
+    void displayData() {
+        cout << "Account Number: " << accNo << endl;
+        cout << "Balance: " << balance << endl;
+    }
+    static void displayMinBalance() {
+        cout << "Minimum Balance: " << minBalance << endl;
+    }
+};
+
+// Initialize the static member minBalance
+double Account::minBalance = 1000.0;
+
+int main() {
+    Account accounts[5];
+
+    // Read data for each account
+    for (int i = 0; i < 5; i++) {
+        cout << "\nEnter details for Account " << i + 1 << endl;
+        accounts[i].readData();
+    }
+
+    // Display data for each account
+    cout << "\nAccount Details:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "\nAccount " << i + 1 << ":" << endl;
+        accounts[i].displayData();
+    }
+
+    // Display the minimum balance using the static member function
+    Account :: displayMinBalance();
+    return 0;
+}
+
+
+/*2.*/
+/*Meters to Feet (Source)*/
+#include <iostream>
+using namespace std;
+class feet_inch{
+    int feet;
+    int inch;
+public:
+    feet_inch(){
+        feet=0;
+        inch=0;
+    }
+    feet_inch(int f,int i){
+        feet=f;
+        inch=i;
+    }
+    void display(){
+        cout << feet << " ft " << inch << " inch " << endl; 
+    }
+};
+class meters{   //Source Class
+    int meter;
+    int centimeter;
+public:
+    meters(int m,int cm){
+        meter=m;
+        centimeter=cm;
+    }
+    operator feet_inch(){
+        feet_inch f1;
+        int f,i;
+        f=meter*3.3;
+        i=centimeter*0.4;
+        f=f+i/12;
+        i=i%12;
+        return feet_inch(f,i);
+    }
+};
+int main(){
+    meters m1(4,50);
+    feet_inch f1;
+    f1=m1;
+    f1.display();
+    return 0;
+}
+
+
+/*Meters to Feet (Destination)*/
+#include <iostream>
+using namespace std;
+class meters{   //Source Class
+    int meter;
+    int centimeter;
+public:
+    meters(int m,int cm){
+        meter=m;
+        centimeter=cm;
+    }
+    int getm(){
+        return meter;
+    }
+    int getcm(){
+        return centimeter;
+    }
+};
+class feet_inch{    //Destination CLass
+    int feet;
+    int inch;
+public:
+    feet_inch(){
+        feet=0;
+        inch=0;
+    }
+    feet_inch(int f,int i){
+        feet=f;
+        inch=i;
+    }
+    feet_inch(meters m1){ 
+        int m,cm;
+        m=m1.getm();
+        cm=m1.getcm();
+        feet=m*3.3;
+        inch=cm*0.4;
+        feet=feet+inch/12;
+        inch=inch%12;
+    }
+    void display(){
+        cout << feet << " ft " << inch << " inch " << endl;
+    }
+};
+int main(){
+    meters m1(4,50);
+    feet_inch f1;
+    f1=m1;
+    f1.display();
+    return 0;
+}
---
+++ Past_qsn_2079.cpp
@@ -0,0 +1,138 @@
+/* 2079 */
+
+// 1.
+#include<iostream>
+using namespace std;
+class author{
+private:
+    string name;
+    string qualification;
+public:
+    void getdata(){
+        cout << "Enter author's name: " ;
+        getline(cin,name);
+        cout << "Enter author's qualification: " ;
+        getline(cin,qualification);
+    }
+    void putdata(){
+        cout << "Author's Name = " << name << endl;
+        cout << "Author's qualification = " << qualification << endl;
+    }
+};
+class publication{
+private:
+    string pname;
+public:
+    void getdata(){
+        cout << "Enter publication name: " ;
+        getline(cin,pname);
+    }
+    void putdata(){
+        cout << "Publication's Name = " << pname << endl;
+    }
+};
+class book : public author, public publication{
+private:
+    string title;
+    int price;
+public:
+    void getdata(){
+        author :: getdata();
+        publication :: getdata();
+
+        cout << "Enter title of book: " ;
+        getline(cin,title);
+        cout << "Enter price of the book: " ;
+        cin >> price;
+    }
+    void putdata(){
+        author :: putdata();
+        publication :: putdata();
+        cout << "Book Title = " << title << endl;
+        cout << "Book Price = " << price << endl;
+    }
+};
+int main(){
+    class book b;
+    // b.author :: getdata();
+    // b.publication :: getdata();
+    b.getdata();
+    cout << endl;
+
+    // b.author :: putdata();
+    // b.publication :: putdata();
+    b.putdata();
+    return 0;
+}
+
+
+//2. 
+#include<iostream>
+using namespace std;
+class dist{
+public:
+    int dis;
+    dist(){
+        dis=0;
+    }
+    void getdistance(){
+        cout << "Enter the distance: " ;
+        cin >> dis;
+    }
+    dist operator > (dist a){
+        if (dis>a.dis)
+            return *this;
+        else
+            return a;
+    }
+};
+int main(){
+    dist d1,d2,d3;
+    d1.getdistance();
+    d2.getdistance();
+    d3 = d1 > d2;
+    cout << "Greater distance is " << d3.dis << endl;
+}
+
+
+//6.
+#include <iostream>
+using namespace std;
+// Inline function to calculate the square of a number
+inline int square(int x) {
+    return x * x;
+}
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // Call the inline function to calculate the square
+    int result = square(num);
+    cout << "The square of " << num << " is: " << result << endl;
+    return 0;
+}
+
+//10.
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    int size;
+    cout << "Enter the size: ";
+    cin >> size;
+
+    int *p = new int[size];
+
+    cout << "Enter the integers: ";
+    for(i=0;i<size;i++){
+        cin >> p[i];
+    }
+
+    cout << "The integers are: ";
+    for(i=0;i<size;i++){
+        cout << p[i] << " ";
+    }
+
+    delete [] p;
+}
