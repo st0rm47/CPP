f905409e391bd44f9c9dd8a4e612e7cf0580a239
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-09-25 20:15:43 +0545

    Lab

--- lab.cpp
+++ lab.cpp
@@ -1,48 +1,48 @@
-// #include<iostream>
-// using namespace std;
-// class bio{  //class
-//     private:
-//     int age;
-//     string name;
-//     float salary;
-//     public:
-//         void getdata(){ // Member function inside a class
-//             cout << "Enter your name: ";
-//             cin >> name;
-//             cout << "Enter your age: ";
-//             cin >> age;
-//             cout << "Enter your salary: ";
-//             cin >> salary;
-//             cout << endl;
-//         }
+#include<iostream>
+using namespace std;
+class bio{  //class
+    private:
+    int age;
+    string name;
+    float salary;
+    public:
+        void getdata(){ // Member function inside a class
+            cout << "Enter your name: ";
+            cin >> name;
+            cout << "Enter your age: ";
+            cin >> age;
+            cout << "Enter your salary: ";
+            cin >> salary;
+            cout << endl;
+        }
     
-//     void data();    //Members of the class
-//     void ssama();   //Members of the class
-// };
-// void bio :: ssama(){    // Member function outside a class
-//     cout << "Salary = " << salary << endl;
+    void data();    //Members of the class
+    void ssama();   //Members of the class
+};
+void bio :: ssama(){    // Member function outside a class
+    cout << "Salary = " << salary << endl;
 
-// }
-// void bio :: data(){     // Member function outside a class
-//     cout << "Name = " << name << endl;
-//     cout << "Age = " << age << endl;
-//     cout << endl;
-// }
+}
+void bio :: data(){     // Member function outside a class
+    cout << "Name = " << name << endl;
+    cout << "Age = " << age << endl;
+    cout << endl;
+}
 
-// int main(){
-// bio mydata[10]; //object using array
+int main(){
+bio mydata[10]; //object using array
 
-// for(int i=0;i<2;i++){
-// mydata[i].getdata();   //Function Call
-// }
-// for(int i=0;i<2;i++){
-// mydata[i].data();      //Function Call
-// }
-// for(int i=0;i<2;i++){
-// mydata[i].ssama();     //Function Call
-// }
+for(int i=0;i<2;i++){
+mydata[i].getdata();   //Function Call
+}
+for(int i=0;i<2;i++){
+mydata[i].data();      //Function Call
+}
+for(int i=0;i<2;i++){
+mydata[i].ssama();     //Function Call
+}
 
-// }
+}
 
 
 #include<iostream>
