538591fa9608cf3ff50e8862990e810cbbb0ad78
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-10-02 13:21:03 +0545

    Lab report 10

--- labreport10.cpp
+++ labreport10.cpp
@@ -29,4 +29,5 @@ int main() {
 /*2. WAP which initializes a string variable to the content "Time is a great teacher but nowadays it is said as Time is Money"
 and outputs the string to disk file OUT.TXT*/
 
+
 /*3. WAP to read content from a text file OUT.TXT and count the number of alphabets present in it.*/
