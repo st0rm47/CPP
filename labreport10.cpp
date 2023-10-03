83f5293e78af4ecb52cb0f8db9b5f63e5630d69a
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-10-03 12:49:21 +0545

    Lab report 10

---
+++ labreport10.cpp
@@ -0,0 +1,6 @@
+/*1. WAP to write 1 to 100 in a data file Notes.txt*/
+
+/*2. WAP which initializes a string variable to the content "Time is a great teacher but nowadays it is said as Time is Money"
+and outputs the string to disk file OUT.TXT*/
+
+/*3. WAP to read content from a text file OUT.TXT and count the number of alphabets present in it.*/
