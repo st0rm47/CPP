16fddcd8d8dce36aae33b5b4db26f81b9fc23c70
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-10-04 13:05:02 +0545

    lAB REPORT 10

--- labreport10.cpp
+++ labreport10.cpp
@@ -1,5 +1,31 @@
 /*1. WAP to write 1 to 100 in a data file Notes.txt*/
 
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    // Open the file in write mode
+    ofstream file("Notes.txt");
+
+    if (file.is_open()) {
+        // Loop from 1 to 100 and write each number followed by a newline character
+        for (int i = 1; i <= 100; ++i) {
+            file << i << "\n";
+        }
+
+        // Close the file
+        file.close();
+        cout << "Data written to Notes.txt successfully." << endl;
+    } else {
+        cerr << "Unable to open Notes.txt for writing." << endl;
+    }
+
+    return 0;
+}
+
+
 /*2. WAP which initializes a string variable to the content "Time is a great teacher but nowadays it is said as Time is Money"
 and outputs the string to disk file OUT.TXT*/
 
