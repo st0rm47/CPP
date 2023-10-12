13ee318b5ee6ecab201b4996571b0671cb674485
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-10-12 19:01:05 +0545

    Lab report

--- Labreport11.cpp
+++ Labreport11.cpp
@@ -1,42 +1,42 @@
-// /*WAP to create a binary file and write data into it.*/
-// #include<iostream>
-// #include<fstream>
-// using namespace std;
-// class test{
-//     int x,y;
-// public:
-//     void getdata(){
-//         cout << "Enter values of x and y: ";
-//         cin >> x >> y;
-//     }
-// };
-// int main(){
-//     test t;
-//     t.getdata();
-//     ofstream a("Data.bin");
-//     a.write((char *)&t,sizeof(t));
-//     a.close();
-//     return 0;
-// }
-
-/*WAP to read the contents from the binary file made recently.*/
+/*WAP to create a binary file and write data into it.*/
 #include<iostream>
 #include<fstream>
 using namespace std;
 class test{
     int x,y;
 public:
-    void showdata(){
-        cout << "The values of x and y: " << endl;
-        cout << x << endl << y;
+    void getdata(){
+        cout << "Enter values of x and y: ";
+        cin >> x >> y;
     }
 };
 int main(){
     test t;
-    ifstream a;
-    a.open("Data.bin");
-    a.read((char *)&t,sizeof(t));
+    t.getdata();
+    ofstream a("Data.bin");
+    a.write((char *)&t,sizeof(t));
     a.close();
-    t.showdata();
     return 0;
 }
+
+// /*WAP to read the contents from the binary file made recently.*/
+// #include<iostream>
+// #include<fstream>
+// using namespace std;
+// class test{
+//     int x,y;
+// public:
+//     void showdata(){
+//         cout << "The values of x and y: " << endl;
+//         cout << x << endl << y;
+//     }
+// };
+// int main(){
+//     test t;
+//     ifstream a;
+//     a.open("Data.bin");
+//     a.read((char *)&t,sizeof(t));
+//     a.close();
+//     t.showdata();
+//     return 0;
+// }
