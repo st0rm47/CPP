0d222d1e507c8d4c1c149036ee0864f36dc74847
Subodh Ghimire <ghimiresubodh59@gmail.com>
2023-11-20 10:59:15 +0545

    Functions

--- 8_functions2.cpp
+++ 8_functions2.cpp
@@ -11,23 +11,23 @@ int main(){
     return 0;
 }
 
-//Overloading of Functions
-// #include<iostream>
-// using namespace std;
-// void mul(int x, int y) {
-//     cout << x*y << endl;
-// }
-// void mul(double x, double y, double z) {
-//     cout << x*y << endl;
-// }
-// void mul(float x, float y){
-//     cout << x*y << endl;
-// }
-// int main(){
-//     mul(3,4);
-//     mul(4.2,3.2,2.02);
-//     mul(3.2f,5.2f);
-// }
+/*-->Overloading of Functions*/
+#include<iostream>
+using namespace std;
+void mul(int x, int y) {
+    cout << x*y << endl;
+}
+void mul(double x, double y, double z) {
+    cout << x*y << endl;
+}
+void mul(float x, float y){
+    cout << x*y << endl;
+}
+int main(){
+    mul(3,4);
+    mul(4.2,3.2,2.02);
+    mul(3.2f,5.2f);
+}
 
 
 
