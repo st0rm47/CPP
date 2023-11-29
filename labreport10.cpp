b6d7c425877240b99009c6392319595c0f0995f6
Subodh Ghimire <ghimiresubodh59@gmail.com>
2023-11-29 12:35:10 +0545

    Lab Report 10

--- labreport10.cpp
+++ labreport10.cpp
@@ -1,165 +1,202 @@
-/*1. WAP to write 1 to 100 in a data file Notes.txt*/
-#include <iostream>
-#include <fstream>
-using namespace std;
+// /*1. WAP to write 1 to 100 in a data file Notes.txt*/
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
 
-int main() {
-    ofstream file;
-    file.open("Notes.txt");
-    for (int i = 1; i <= 100; i++){
-            file << i << endl;
-        }
-    file.close();
-    return 0;
-}
+// int main() {
+//     ofstream file;
+//     file.open("Notes.txt");
+//     for (int i = 1; i <= 100; i++){
+//             file << i << endl;
+//         }
+//     file.close();
+//     return 0;
+// }
 
 
-/*2. WAP which initializes a string variable to the content "Time is a great teacher but nowadays it is said as Time is Money"
-and outputs the string to disk file OUT.TXT*/
-#include<iostream>
-#include<fstream>
-#include<string>
-using namespace std;
+// /*2. WAP which initializes a string variable to the content "Time is a great teacher but nowadays it is said as Time is Money"
+// and outputs the string to disk file OUT.TXT*/
+// #include<iostream>
+// #include<fstream>
+// #include<string>
+// using namespace std;
 
-int main(){
-    string a = "Time is a great teacher but nowadays it is said as Time is money";
-    ofstream file;
-    file.open("OUT.txt");
-    file << a << endl; 
-    file.close();
-    return 0;
-}
+// int main(){
+//     string a = "Time is a great teacher but nowadays it is said as Time is money";
+//     ofstream file;
+//     file.open("OUT.txt");
+//     file << a << endl; 
+//     file.close();
+//     return 0;
+// }
 
-/*3. WAP to read content from a text file OUT.TXT and count the number of alphabets present in it.*/
-#include<iostream>
-#include<fstream>
-using namespace std;
+// /*3. WAP to read content from a text file OUT.TXT and count the number of alphabets present in it.*/
+// #include<iostream>
+// #include<fstream>
+// using namespace std;
 
-int main(){
-    int c = 0;
-    char x;
-    ifstream file;
-    file.open("OUT.txt");
-    cout << "String in file : ";
-    while (file.get(x)){
-        cout << x;
-        if(x >= 'A' && x <= 'Z' || x >='a' && x <='z')
-        c++;
-    }
-    file.close();
-    cout << "Total number of alphabets : " << c << endl;
-    return 0;
-}
+// int main(){
+//     int c = 0;
+//     char x;
+//     ifstream file;
+//     file.open("OUT.txt");
+//     cout << "String in file : ";
+//     while (file.get(x)){
+//         cout << x;
+//         if(x >= 'A' && x <= 'Z' || x >='a' && x <='z')
+//         c++;
+//     }
+//     file.close();
+//     cout << "Total number of alphabets : " << c << endl;
+//     return 0;
+// }
 
-/*4. WAP to write and read values using variables in/from a file. */
-#include<iostream>
-#include<fstream>
-using namespace std;
+// /*4. WAP to write and read values using variables in/from a file. */
+// #include<iostream>
+// #include<fstream>
+// using namespace std;
 
-int main(){
-    char name[30];
-    int roll;
-    fstream file;
-    file.open("Data.txt",ios::out);
-    cout << "Enter a name: ";
-    cin >> name;
-    file << name << endl;
-    cout << "Enter roll number: ";
-    cin >> roll;
-    file << roll << endl;
-    file.close();
+// int main(){
+//     char name[30];
+//     int roll;
+//     fstream file;
+//     file.open("Data.txt",ios::out);
+//     cout << "Enter a name: ";
+//     cin >> name;
+//     file << name << endl;
+//     cout << "Enter roll number: ";
+//     cin >> roll;
+//     file << roll << endl;
+//     file.close();
 
-    file.open("Data.txt",ios::in);
-    file >> name;
-    file >> roll;
-    cout << endl;
-    cout << "Name: " << name << endl;
-    cout << "Roll Number: " << roll << endl;
-    file.close();
-    return 0;
-}
+//     file.open("Data.txt",ios::in);
+//     file >> name;
+//     file >> roll;
+//     cout << endl;
+//     cout << "Name: " << name << endl;
+//     cout << "Roll Number: " << roll << endl;
+//     file.close();
+//     return 0;
+// }
 
 
 /*5. WAP to find the sum of two complex numbers by overloading << and >> stream operators*/
-#include <iostream>
-#include <fstream>
-using namespace std;
-class complex {
-public:
-    int a,b;
-    complex(){
-        a = 0;
-        b = 0;
-    }
-    complex operator+( complex& x) {
-        complex temp;
-        temp.a = a + x.a;
-        temp.b = b + x.b;
-        return temp;
-    }
-    friend istream & operator >> (istream & in, complex & c);
-    friend ostream & operator << (ostream & out, complex & c);
-};
-istream & operator >> (istream & in, complex & c){
-    cout << "Real part: ";
-    cin >> c.a;
-    cout << "Imaginary part: ";
-    cin >> c.b;
-    return in;
-}
-ostream & operator << (ostream & out, complex & c){
-    cout << c.a << " + " << c.b << "i" << endl;
-    return out;
-}
-int main(){
-    complex c1,c2,sum;
-    cout << "Enter first complex number: " << endl;
-    cin >> c1;
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
+// class complex {
+// public:
+//     int a,b;
+//     complex(){
+//         a = 0;
+//         b = 0;
+//     }
+//     complex operator+(complex& x) {
+//         complex temp;
+//         temp.a = a + x.a;
+//         temp.b = b + x.b;
+//         return temp;
+//     }
+//     friend istream& operator >> (istream&  in, complex&  c);
+//     friend ostream& operator << (ostream& out, complex&  c);
+// };
+// istream& operator >> (istream& in, complex& c){
+//     cout << "Real part: ";
+//     cin >> c.a;
+//     cout << "Imaginary part: ";
+//     cin >> c.b;
+//     return in;
+// }
+// ostream& operator << (ostream& out, complex& c){
+//     cout << c.a << " + " << c.b << "i" << endl;
+//     return out;
+// }
+// int main(){
+//     complex c1,c2,sum;
+//     cout << "Enter first complex number: " << endl;
+//     cin >> c1;
 
-    cout << "Enter second complex number: " << endl;
-    cin >> c2;
+//     cout << "Enter second complex number: " << endl;
+//     cin >> c2;
 
-    sum = c1 + c2;
-    cout << "The sum of two complex number is: " << sum << endl;
-    return 0;
-}
+//     sum = c1 + c2;
+//     cout << "The sum of two complex number is: " << sum << endl;
+//     return 0;
+// }
 
 /*6. WAP demonstrating example of tellg(), tellp(), seekg(), seekp().*/
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
+
+// int main() {
+//     fstream file("file.txt", ios::in | ios::out | ios::trunc);
+
+//     // Write data to the file
+//     file << "Hello" << endl;
+
+//     // Get and display the current positions of the input and output file pointers
+//     cout << "Initial positions:" << endl;
+//     cout << "Input Position: " << file.tellg() << endl;
+//     cout << "Output Position: " << file.tellp() << endl;
+
+//     // Seek to the beginning of the file
+//     file.seekg(0, ios::beg);
+//     file.seekp(5, ios::beg);
+
+//     //Read from the current input position
+//     string line;
+//     getline(file, line);
+//     cout << "Read from input position: " << line << endl;
+
+//     // Seek to the end of the file
+//     file.seekp(0, ios::end);
+
+//     // Write more data to the end of the file
+//     file << "Nepal" << endl;
+
+//     // Get and display the updated positions of the input and output file pointers
+//     cout << "Updated positions:" << endl;
+//     cout << "Input Position: " << file.tellg() << endl;
+//     cout << "Output Position: " << file.tellp() << endl;
+//     file.close();
+//     return 0;
+// }
+
+
 #include <iostream>
 #include <fstream>
 using namespace std;
-
 int main() {
-    fstream file("file.txt", ios::in | ios::out | ios::trunc);
+    fstream file("example.txt",ios::out | ios::in);
 
-    // Write data to the file
-    file << "Hello" << endl;
+    file << "Hello, World!" << endl;
 
-    // Get and display the current positions of the input and output file pointers
-    cout << "Initial positions:" << endl;
-    cout << "Input Position: " << file.tellg() << endl;
-    cout << "Output Position: " << file.tellp() << endl;
+    // Display the initial position of the output file pointer
+    cout << "Initial Output Position: " << file.tellp() << endl;
 
-    // Seek to the beginning of the file
-    file.seekg(0, ios::beg);
-    file.seekp(5, ios::beg);
+    // Seek to the 7th position from the beginning of the file
+    file.seekp(7, ios::beg);
 
-    // Read from the current input position
-    string line;
-    getline(file, line);
-    cout << "Read from input position: " << line << endl;
+    // Display the updated position of the output file pointer
+    cout << "Updated Output Position: " << file.tellp() << endl;
 
-    // Seek to the end of the file
-    file.seekp(0, ios::end);
+    // Display the initial position of the input file pointer
+    cout << "Initial Input Position: " << file.tellg() << endl;
 
-    // Write more data to the end of the file
-    file << "Nepal" << endl;
+    // Seek to the 2nd position from the beginning of the file
+    file.seekg(2, ios::beg);
 
-    // Get and display the updated positions of the input and output file pointers
-    cout << "Updated positions:" << endl;
-    cout << "Input Position: " << file.tellg() << endl;
-    cout << "Output Position: " << file.tellp() << endl;
+    // Display the updated position of the input file pointer
+    cout << "Updated Input Position: " << file.tellg() << endl;
+
+    // Read and display the content from the current input position
+    string content;
+    getline(file, content);
+    cout << "Read from Input Position: " << content << endl;
+
     file.close();
+
     return 0;
 }
 
